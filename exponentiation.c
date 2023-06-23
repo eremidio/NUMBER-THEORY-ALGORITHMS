5cf9c2de5a2fd4428f59180052bdedc057c9c6a4
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-06-23 00:32:48 -0300

    Add files via upload

---
+++ exponentiation.c
@@ -0,0 +1,33 @@
+//UM PROGRAMA QUE TESTA AS ROTINAS DEFINIDAS NOS ARQUIVO mod_bin_exponentiation,h
+//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o exponentiation exponentiation.c -lm
+
+//Cabeçalho
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include"mod_bin_exponentiation.h"
+
+//Função principal
+
+int main(){
+//Variáveis locais
+int a, b, n;
+int bin_array_size = 0;
+//Procedimentos
+//Recebendo input do usuário
+printf("Digite uma base:\n->");
+scanf("%d", &a);
+printf("Digite um expoente:\n->");
+scanf("%d", &b);
+printf("Digite um inteiro congruente:\n->");
+scanf("%d", &n);
+//Testes
+printf("Resultado biblioteca <math.h>: %d\n", ((int)pow(a, b)%n));
+printf("Exponencial modular: %lu\n", mod_pow(a, b, n));
+printf("Exponencial binária: %lu\n", bin_pow(a, b));
+printf("Exponencial binária modular: %lu\n", mod_bin_pow(a, b, n));
+
+//Finalizando a aplicação
+return 0;
+          }
+
