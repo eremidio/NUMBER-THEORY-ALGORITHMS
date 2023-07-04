6f7bd5f1af938c067d258f9ce3342d5b868f0d33
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-07-03 23:45:50 -0300

    Add files via upload

---
+++ wilson_prime_test.c
@@ -0,0 +1,54 @@
+//VAM0S IMPLEMENTAR UM PROGRAMA EM C QUE IMPLEMENTA O TEOREMA DE WILSON PARA DECIDIR SE UM NÚMERO É PRIMO
+//COMPILAR ESTEPROGRAMA COM O COMANDO: gcc -o wilson_prime_test wilson_prime_test.c
+
+/*
+O CHAMADO TEOREMA DE WILSON AFIRMA QUE UM NÚMERO p É PRIMO SE E SOMENTE SE (p-1)!=(p-1) mod(p).
+ESTE TEOREMA FORNECEUM TESTE EXTREMAMENTE BÁSICO PARA TESTAR A PRIMALIDADE DEUM NÚMERO.
+
+*/
+
+//**************************************************************************************************************************
+//CABEÇALHO
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<stdbool.h>
+
+
+//**************************************************************************************************************************
+//FUNÇÕES
+bool wilson_test(uint64_t n){
+//Variáveis locais
+uint64_t result=1;
+//Procedimentos
+for(uint64_t i=2; i<n; ++i)
+result=(result*i)%n;
+
+if(result==(n-1))
+return true;
+else
+return false;
+                            };
+
+
+//**************************************************************************************************************************
+//FUNÇÃO PRINCIPAL
+int main(){
+//Variáveis locais
+uint64_t number, i;
+
+//Procedimentos
+//Recebendo um input do usuário
+printf("Digite um limitante para sua sequência de números primos: ");
+scanf("%lu", &number);
+
+//Printando uma sequência de números primos
+for(i=2; i<=number; ++i){
+if(wilson_test(i)==true)
+printf("%lu, ",i);
+                        };
+printf("\n");
+
+//Finalizando a aplicação
+return 0;
+          }
