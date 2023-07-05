5ab3f3ae31ee7e643afac765c918d06793e686d2
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-07-05 02:48:22 -0300

    Update numero_primo.cpp

--- numero_primo.cpp
+++ numero_primo.cpp
@@ -11,7 +11,21 @@ bool eprimo(uint64_t n){
 uint64_t i;
 
 //Procedimentos
+//Casos triviais
+if(n==2 || n==3 || n==5 || n==7 || n==11 ||  n==13|| n==17 || n==19 || n==23 || n==29|| n==31 || n==37 || n==41 || n==43)
+return true;
+if(n==47 || n==53 || n==59 || n==61 || n==67 || n==71|| n==73 || n==79 || n==83 || n==89|| n==97)
+return true;
 
+//Teste de fatores primos menores que 100
+if(n%2==0 || n%3==0 || n%5==0 || n%7==0 || n%11==0|| n%13==0|| n%17==0 || n%19==0 || n%23==0 || n%29==0|| n%31==0 || n%37==0 || n%41==0)
+return false;
+else if(n%43==0|| n%47==0 || n%53==0 || n%59==0|| n%61==0 || n%67==0 || n%71==0 || n%73==0|| n%79==0 || n%83==0 || n%89==0)
+return false;
+else if(n%97==0)
+return false;
+
+//Tentativa de divisão e erro com uma otimização 6k+1
 if (n == 2 || n == 3)
 return true;
 
@@ -22,7 +36,7 @@ for(i = 5; i * i <= n; i += 6){
 if (n % i == 0 || n % (i + 2) == 0)
 return false;
                              };
- return true;
+return true;
                        }
 
 //FUNÇÃO PRINCIPAL
