2fdcc72934560d5c6203e5105c47120bf93ecc57
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-07-24 23:53:26 -0300

    Add files via upload

---
+++ integer_partition.c
@@ -0,0 +1,27 @@
+//VAMOS CRIAR UM PROGRAMA PARA TESTAR AS ROTINAS CRIADAS NO ARQUIVO integer_partition.h
+//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o integer_partition integer_partition.c -lm
+
+//Cabeçalho
+#include<stdio.h>
+#include"integer_partition.h"
+
+
+
+//Função principal
+int main(){
+//Variáveis locais
+unsigned int number;
+
+//Procedimentos
+//Recebendo input do usuário
+printf("Digite um número inteiro: ");
+scanf("%u", &number);
+
+//Teste de funções
+//Função de partição
+printf("Soma dos divisores de %u: %u\n", number, sum_of_divisors(number));
+printf("Número de partições de %u: %u\n", number, partition_function(number));
+
+//Finalizando a aplicação
+return 0;
+          }
