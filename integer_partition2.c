58a4cb639b7c8b379e1914c51d7866dc9d995f6b
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-07-26 14:35:43 -0300

    Add files via upload

---
+++ integer_partition2.c
@@ -0,0 +1,23 @@
+//VAMOS CRIAR UM PROGRAMA QUE TESTA A ROTINA CRIADA NO ARQUIVO integer_partition2.h
+//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o integer_partition2 integer_partition2.c 
+
+
+//Cabeçalho
+#include<stdio.h>
+#include"integer_partition2.h"
+
+
+//Função principal
+int main(){
+//Variáveis locais
+int number;
+//Procedimentos
+//Recebendo input do usuário
+printf("Usuário digite um número inteiro:\n-> ");
+scanf("%d", &number);
+//Cálculo das partições de um inteiro
+generate_partition(number, Yes);
+
+//Finalizando a aplicação
+return 0;
+          }
