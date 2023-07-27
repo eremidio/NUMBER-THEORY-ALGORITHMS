06c9d346154482764e024197d28153c1d672984e
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-07-27 14:22:06 -0300

    Add files via upload

---
+++ fermat_number.c
@@ -0,0 +1,89 @@
+//VAMOS CRIAR UM PROGRAMA QUE IMPLEMENTA A CLASSE DOS NÚMEROS DE FERMAT E O TESTE DE PÉPIN PARA A PRIMALIDADE DOS TESTE DE FERMAT
+//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o fermat_number fermat_number.c
+
+/*
+UM NÚMERO DE FERMAT É UM NÚMERO INTEIRO DA FORMA: F(n)=2^(2^n)+1.
+
+VÁRIAS RELAÇÕES DE RECORRÊNCIA EXISTEM ENVOLVENDO ESTES NÚMEROS, POR EXEMPLO: 
+PARA n>1: F(n)=(F(n-1)-1)²+1 E F(n)=F(0).F(1)...F(n-1)+2
+
+O CHAMADO TESTE DE PÉPIN PODE SER USADO PARA DETERMINAR SE UM NÚMERO DE FERMAT É PRIMO OU NÃO.
+ESTE TEOREMA BASICAMENTE AFIRMA QUE PARA UM NÚMERO DE FERMAT SER PRIMO ELE TEM DE SATISFAZER A RELAÇÃO: 
+3^((F(n)-1)/2)=(F(n)-1) mod(F(n))
+
+PARA MAIORES INFORMAÇÕES: https://en.wikipedia.org/wiki/Pépin%27s_test
+                          https://en.wikipedia.org/wiki/Fermat_number
+
+*/
+
+//***************************************************************************************************************************
+//CABEÇALHO
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdint.h>
+#include"mod_bin_exponentiation.h"
+//***************************************************************************************************************************
+//FUNÇÕES
+//Função que determina se um número inteiro positivo é um número de Fermat
+bool is_fermat_number(uint64_t n){
+//Caso trivial
+if(n==3)
+return true;
+if(n<3)
+return false;
+
+//Variáveis locais
+uint64_t fermat, result;//Primeiro número na sequência
+
+//Ajuste de variáveis
+fermat=3;
+result=0;
+
+//Procedimento
+while(result<n){
+result=((fermat-1)*(fermat-1))-1;
+//Atualizando variáveis para pr[oxim iteração
+fermat = result;
+               };
+
+//Resultado
+return (result==n);
+                                 };
+
+//Teste de Pépin para testar a primalidade de um número
+void pepin_test(uint64_t n){
+//Checando se o número em questão é um número de Fermat
+bool test1=is_fermat_number(n);
+if(test1==false){
+printf("O número em questão não é um número de Fermat!\n");
+return;
+               };
+
+//Teste de primalidade
+uint64_t exponent=(n-1)/2;
+uint64_t tester=mod_pow(3, exponent, n);
+
+if(tester==(n-1))
+printf("O número em questão é primo.\n");
+else
+printf("O número em questão não é primo.\n");
+
+                           };
+
+
+//***************************************************************************************************************************
+//FUNÇÃO PRINCIPAL
+int main(){
+//Variáveis locais
+uint64_t n;
+
+//Procedimentos
+//Recebendo input do usuário
+printf("Digite um número inteiro: ");
+scanf("%lu", &n);
+
+//Aplicando o teste de Pépin
+pepin_test(n);
+//Finalizando a aplicação
+return 0;
+          }
