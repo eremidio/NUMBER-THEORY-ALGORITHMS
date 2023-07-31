4d85ee56a22550aa5d16dc1f4dc4322143200ca9
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-07-31 18:20:13 -0300

    Add files via upload

---
+++ farey_sequence.cpp
@@ -0,0 +1,30 @@
+//VAMOS CRIAR UM PROGRAMA PARA TESTAR AS ROTINAS CRIADAS NO ARQUIVO farey_sequence.h
+//COMPILAR ESTE PROGRAMA COM O COMANDO: g++ -o farey_sequence farey_sequence.cpp
+
+
+//Cabeçalho
+#include"farey_sequence.hpp"
+#include<iostream>
+using namespace std;
+
+//Função principal
+int main(){
+//Variáveis locais
+int n;
+//Procedimento
+//Recebendo input do usuário
+cout<<"Digite um número inteiro: ";
+cin>>n;
+
+//Gerando as sequências de Farey
+cout<<"Ordem crescente:\n";
+for(int i=1; i<=n; ++i)
+generate_farey_sequence(i, Up);
+cout<<"Ordem decrescente:\n";
+for(int i=1; i<=n; ++i)
+generate_farey_sequence(i, Down);
+                       
+//Finalizando a aplicação
+return 0;
+          }
+ 
