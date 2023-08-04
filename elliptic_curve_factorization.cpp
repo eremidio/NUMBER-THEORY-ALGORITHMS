20a04a34961104012a08d3713b1d1ff39fd4b765
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-08-04 10:05:04 -0300

    Add files via upload

---
+++ elliptic_curve_factorization.cpp
@@ -0,0 +1,20 @@
+//VAMOS CRIAR UM PROGRAMA PARA TESTAR AS ROTINAS DEFINIDAS NO ARQUIVO elliptic_curve_factorization.hpp
+//COMPILAR ESTE PROGRAMA COM: g++ -o elliptic_curve_factorization elliptic_curve_factorization.cpp
+
+//CABEÇALHO
+#include"elliptic_curve_factorization.hpp"
+using namespace std;
+
+//FUNÇÃO PRINCIPAL
+int main(){
+//Instanciando um objeto da classe e executando um algoritmo
+elliptic_curve_method factorizer;
+factorizer.run();
+
+//TESTES USE UM // APÓS OS TESTES 
+//cout<<modular_inverse(73, 75)<<"\n";
+
+//Finalizando a aplicação
+return 0;
+
+          }
