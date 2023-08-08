8ffafb197cc57fedec08395b9313a3cff109f8d9
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-08-08 11:14:09 -0300

    Update rational_sieve.hpp

--- rational_sieve.hpp
+++ rational_sieve.hpp
@@ -30,7 +30,7 @@ https://en.wikipedia.org/wiki/Rational_sieve
 #include<stdint.h>
 
 //VARIÁVEIS GLOBAIS
-const uint64_t prime_seed[] ={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+const uint64_t prime_seed[] ={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997};
 
 //**************************************************************************************************************************
 //FUNÇÕES AUXILIARES
@@ -52,7 +52,7 @@ std::cout<<"}\n";
                                                    };
 
 
-//Função que calcula se um número é um quadrado perfeito
+//Função que calcula de um número é um quadrado perfeito
 bool is_square(uint64_t n){
 //Procedimentos
 for(uint64_t i=2; ; ++i){
@@ -125,7 +125,7 @@ return;
 void rational_sieve::setup(){
 std::cout<<"Digite um número inteiro para ser fatorado:\n->";
 std::cin>> number;
-std::cout<<"Digite um número de 1 a 25 para selecionar a profundidade da busca por fatores primos:\n->";
+std::cout<<"Digite um número de 1 a 168 para selecionar a profundidade da busca por fatores primos:\n->";
 std::cin>> smoothness;
 
 //Preenchendo o vetor de base de primos
@@ -190,7 +190,7 @@ number_vector2.push_back(i+number);
                                   };
 
 //Fim da busca por candidatos para as relações constitutivas
-if(counter==smoothness)
+if(counter==(smoothness+5))
 break;
                        };
 
