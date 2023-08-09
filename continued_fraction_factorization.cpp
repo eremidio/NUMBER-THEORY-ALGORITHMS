c4ac6a5f671a1ac015bc91c4dd18e94dc81ec34a
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-08-08 23:40:42 -0300

    Add files via upload

---
+++ continued_fraction_factorization.cpp
@@ -0,0 +1,16 @@
+//VAMOS IMPLMENTAR UM  PROGRAMA PARA TESTAR AS ROTINAS CRIADAS  NO ARQUIVO continued_fraction_factorization.hpp
+//COMPILAR ESTE PROGRAMA COM: g++ -o continued_fraction_factorization continued_fraction_factorization.cpp
+
+
+//Cabeçalho
+#include"continued_fraction_factorization.hpp"
+using namespace std;
+
+
+//Função principal
+int main(){
+continued_fraction_factorization factorizer;
+factorizer.run();
+//Finalizando a aplicação
+return 0;
+          }
