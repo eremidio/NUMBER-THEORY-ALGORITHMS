266601c88a43e8fe5cefd7e6e6c29dfc27962840
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-08-23 00:32:12 -0300

    Update sundaram_sieve.c

--- sundaram_sieve.c
+++ sundaram_sieve.c
@@ -3,16 +3,34 @@
 
 /*CABEÇALHO*/
 #include<stdbool.h>
+#include<stdint.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 
+
+//Função que determina se um  número é quadrado perfeito
+bool perfect_square_check(int64_t n){
+//Variáveis locais
+int64_t odd=1;
+//Procedimentos
+while(n>0){
+n-=odd;
+odd+=2;
+if(n<0)
+return false;
+          };
+
+return true;
+                                    };
+
+
 //FUNÇÃO QUE APLICA A PENEIRA DE SUNDARAM PARA UMA SEQUÊNCIA DE INTEIROS ATÉ UM DADO VALOR
-void sundaram_sieve(int n){
+void sundaram_sieve(int64_t n){
 //VARIÁVEIS LOCAIS
 bool number_list[n+1];
-int i, j, k;
-int ground;
+int64_t i, j, k;
+int64_t ground;
 double sqrt_n;
 
 //PROCEDIMENTOS
@@ -42,14 +60,8 @@ number_list[i+j+2*i*j]=false;
 //PRINTANDO A LISTA DE NÚMEROS PRIMOS
 printf("2, 3, ");
 for(k=2; (2*k+1)<n; ++k){
-
-for(i=0; i<ground; ++i){
-if(i*i==2*k+1)
-continue;
-                       };
-
-if(number_list[k]==true)
-printf("%d, ", (2*k+1));
+if(number_list[k]==true && perfect_square_check((2*k+1))==false)
+printf("%li, ", (2*k+1));
                   };
                           };
 
@@ -57,15 +69,14 @@ printf("%d, ", (2*k+1));
 //FUNÇÃO PRINCIPAL
 int main(){
 //DEFINIÇÃO DE VARIÁVEIS
-int n;
+int64_t n;
 //RECEBENDO INPUT DO USUÁRIO E EXIBINDO OS NÚMEROS PRIMOS
 printf("Vamos calcular os números primos até um certo valor usando a peneira de Sundaram.\n");
 printf("Usuário digite um número inteiro:\n");
-scanf("%d", &n);
+scanf("%li", &n);
 sundaram_sieve(n);
 printf("\n");
 
 //FINALIZANDO A APLICAÇÃO
 return 0;
           }
-
