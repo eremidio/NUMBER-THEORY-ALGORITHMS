971e0a2d7db62a93c66818d303327711696530c3
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-09-19 13:27:20 -0300

    Add files via upload

---
+++ ramanujan_sum.c
@@ -0,0 +1,25 @@
+//VAMOS CRIAR UM PROGRAMA QUE TESTA AS ROTINAS CRIADAS NO ARQUIVO ramanujan_tau_function.h
+//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o ramanujan_sum ramanujan_sum.c -lm
+
+//Cabeçalho
+#include<stdio.h>
+#include"ramanujan_sum.h"
+
+//Função principal
+//Função principal
+int main(){
+//Variáveis locais
+int64_t n, q;
+//Procedimentos
+//Recebendo input do usuário
+printf("Usuário digite um número inteiro: ");
+scanf("%li", &q);
+printf("Usuário digite um número inteiro: ");
+scanf("%li", &n);
+
+//Calculando as funções de Möbius e as funções de Mertens
+printf("c(%li, %li) = %li\n", q, n, ramanujan_sum(q, n));
+
+//Finalizando a aplicação
+return 0;
+          }; 
