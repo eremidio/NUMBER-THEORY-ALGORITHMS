4ea0329f8a9560fea00012183d6c7e0c18df2d85
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-10-27 04:40:33 -0300

    Update pocklington_primality_test.py

--- pocklington_primality_test.py
+++ pocklington_primality_test.py
@@ -131,12 +131,10 @@ def reduce_number(n:int):
  #Redução por fatores primos inferiores a 1000 usando o algoritmo de Euclides
  common_factor_list:list=[2305567963945518424753102147331756070, 3383080509296917481189798760796480670771162183, 262025664754470334382813071883984477441, 49766523393936228750138598082752980119549, 1227453540237014997887615651109181957903188941, 253056306993037840092244562196981381959, 879031892818978804933065306274691112009314693, 16866467815653776127243907167629319108817, 83623435734606723958852552002152901629917681, 500139990716305530696933027389294139003181]
 
- for count in range(1001):
-  for x in common_factor_list:
-   common_reducer:int=euclides_algorithm(x, n)
-   if(common_reducer>1):
-    n//=common_reducer
-
+ for x in common_factor_list:
+  common_reducer:int=euclides_algorithm(x, n)
+  if(common_reducer>1):
+   n//=common_reducer
 
  #Variáveis locais
  limit:int=int(math.sqrt(n))
@@ -151,10 +149,10 @@ def reduce_number(n:int):
    n//=(y+2)
 
   if(y>limit2 and n<current_value):
-   return int(n)
+   return n
 
  #Resultado
- return int(n)
+ return n
 
 
 '''TESTE USE UM # APÓS O MESMO'''
