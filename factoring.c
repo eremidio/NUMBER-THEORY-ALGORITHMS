8d095a9d989e3cb52278ae7017eac1bbdf562a2f
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-11-22 14:27:39 -0300

    Add files via upload

---
+++ factoring.c
@@ -0,0 +1,106 @@
+//VAMOS ESCREVER UM NÚMERO QUE EXIBE NA TELA A DECOMPOSIÇÃO DE UM NÚMERO EM SEUS FATORES PRIMOS
+//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o  factoring factoring.c -lm -O2
+
+//****************************************************************************************************************************
+//CABEÇALHO
+#include<stdio.h>
+#include<stdint.h>
+#include<math.h>
+
+//****************************************************************************************************************************
+//DECLARAÇÃO DE FUNÇÕES
+void factorize(uint64_t);
+
+//****************************************************************************************************************************
+//FUNÇÕES
+//Função que calcula os fatores primos de um número usaremos uma otimização 30k+1
+void factorize(uint64_t n){
+//Variáveis locais
+uint64_t i;
+uint64_t limit = sqrt(n);
+uint64_t divisors[7]={1, 7, 11, 13, 17, 23, 29};//Números coprimos com 30 no intervalo 1,...,30
+
+//Procedimentos
+printf("Eis os fatores primos de %lu:\n", n);
+
+//Busca por fatores 2, 3, 5
+while((n%2)==0){
+printf("2, ");
+n/=2;
+               };
+
+while((n%3)==0){
+printf("3, ");
+n/=3;
+               };
+
+while((n%5)==0){
+printf("5, ");
+n/=5;
+               };
+
+//Condição que detecta o fim do algoritmo
+if(n==1){
+printf("...\n");
+return;
+        };
+
+//Loop principal
+//1º iteração
+for(i=1; i<7; ++i){
+while((n%divisors[i])==0){
+printf("%lu, ", divisors[i]);
+n/=divisors[i];
+                         };
+                  };
+
+//Condição que detecta o fim do algoritmo
+if(n==1){
+printf("...\n");
+return;
+        };
+
+//Demais interações
+while(divisors[6]<limit){
+//Atualizando variáveis
+for(i=0; i<7; ++i)
+divisors[i]=divisors[i]+30;
+
+//Testando possíveis divisores
+for(i=0; i<7; ++i){
+while((n%divisors[i])==0){
+printf("%lu, ", divisors[i]);
+n/=divisors[i];
+                         };
+                  };
+
+//Condição que detecta o fim do algoritmo
+if(n==1){
+printf("...\n");
+return;
+        };
+                        };
+
+//Fatores superiores a n^(1/2)
+printf("%lu, ...\n", n);
+return;
+
+                          };
+
+//FUNÇÃO PRINCIPAL
+int main(){
+//Variáveis locais
+uint64_t number;
+//Procedimentos
+//Recebendo input do usuário
+printf("Digite um número para ser fatorado: ");
+if(scanf("%lu", &number)!= 1)
+return 1;
+
+//Fatorando o número
+factorize(number);
+
+//Finalizando a aplicação
+return 0;
+
+          };
