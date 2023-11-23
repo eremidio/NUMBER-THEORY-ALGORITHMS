84e1ee64670821ede70fe6d229cc8b27bb755427
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-11-23 01:37:36 -0300

    Update elliptic_curve_factorization.cpp

--- elliptic_curve_factorization.cpp
+++ elliptic_curve_factorization.cpp
@@ -1,5 +1,5 @@
 //VAMOS CRIAR UM PROGRAMA PARA TESTAR AS ROTINAS DEFINIDAS NO ARQUIVO elliptic_curve_factorization.hpp
-//COMPILAR ESTE PROGRAMA COM: g++ -o elliptic_curve_factorization elliptic_curve_factorization.cpp -O2
+//COMPILAR ESTE PROGRAMA COM: g++ -o elliptic_curve_factorization elliptic_curve_factorization.cpp -O2 -lboost_system
 
 //CABEÇALHO
 #include"elliptic_curve_factorization.hpp"
@@ -10,9 +10,6 @@ int main(){
 //Instanciando um objeto da classe e executando um algoritmo
 elliptic_curve_method factorizer;
 factorizer.run();
-
-//TESTES USE UM // APÓS OS TESTES 
-//cout<<modular_inverse(73, 75)<<"\n";
 
 //Finalizando a aplicação
 return 0;
