4c70dc8596fd710b705dce5c5759cb9d3cec06ca
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-11-24 18:29:34 -0300

    Update selfridge_prime_test.c

--- selfridge_prime_test.c
+++ selfridge_prime_test.c
@@ -1,5 +1,5 @@
 //VAMOS IMPLEMENTAR UM PROGRAMA QUE IMPLEMENTA O ALGORITMO DE SELFRIDGE PARA TESTAR SE UM NÚMERO É PRIMO OU NÃO
-//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o selfridge_prime_test selfridge_prime_test.c
+//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o selfridge_prime_test selfridge_prime_test.c 
 
 /*
 O TESTE DE SELFRIDGE É BASEADO EM UMA CONJECTURA QUE AFIRMA QUE UM NÚMERO p É PRIMO SE E SOMENTE SE:
@@ -20,19 +20,30 @@ ESTE É UM REASULTADO AINDA NÃO COMPROVADO, EMBORA FUNCIONE RAZOALMENTE BEM EM 
 
 //********************************************************************************************************************
 //FUNÇÕES
-
 //Função que calcula o n-ésimo número na sequência de Fibonacci a menos de uma congruência
 long long int fibonacci_module(long long int n, long long int p){
-//Definição
-if(n==1 || n==2)
-return 1;
-else
-return (fibonacci_module(n-1, p)+fibonacci_module(n-2, p))%p;
+//Variáveis locais
+long long int f1=1, f2=1, f3;
+long long int counter=3;
+
+//Procedimentos
+//Loop principal
+while(counter<p){
+f3=(f1+f2)%p;
+
+//Atualizando variáveis para a próxima iteração
+f1=f2;
+f2=f3;
+counter++;
+                };
+
+//Resultado
+return f3;
                                                                 };
 
 //Teste de primalidade
 bool selfridge_test(long long int n){
-if(fibonacci_module(n+1, n)==0 && mod_pow(2, n-1, n)==1)
+if(fibonacci_module(n+1, n)==0 && mod_bin_pow(2, n-1, n)==1)
 return true;
 else 
 return false;
@@ -61,5 +72,4 @@ printf("...\n");
 //Finalizando a aplicação
 return 0;
           }
-
 
