30199c41671058f53cf646c6f8f8325a8e37fa3b
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-11-28 02:50:10 -0300

    Add files via upload

---
+++ montgomery_elliptic_curve_factorization.cpp
@@ -0,0 +1,17 @@
+//VAMOS CRIAR UM PROGRAMA PARA TESTAR AS ROTINAS CRIADAS NO ARQUIVO montgomery_elliptic_curve_factorization.hpp
+//COMPILAR ESTE PROGRAMA COM O COMANDO: g++ -o montgomery_elliptic_curve_factorization montgomery_elliptic_curve_factorization.cpp -O2 -lboost_system
+
+//Cabeçalho
+#include"montgomery_elliptic_curve_factorization.hpp"
+
+//Função principal
+int main(){
+//Criando um objeto da classe
+elliptic_curve_method factorizer;
+
+//Executando o algoritmo
+factorizer.run();
+
+//Finalizando a aplicação
+return 0;
+          }
