43deb35982a6b69236e70e0e9ab56184bdef7460
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-12-14 02:21:19 -0300

    Update jacobi_symbol.h

--- jacobi_symbol.h
+++ jacobi_symbol.h
@@ -1,4 +1,4 @@
-//VAMOS IMPLEMENTAR UMA FUNÇÃO QUE CALCULAO SÍMBOLO DE JACOBI
+//VAMOS IMPLEMENTAR UMA FUNÇÃO QUE CALCULA O SÍMBOLO DE JACOBI
 
 /*
 O CHAMADO SÍMBOLOS DE JACOBI SÃO UMA GENERALIZAÇÃO DOS SÍMBOLOS DE LEGENDRE.
@@ -7,15 +7,13 @@ O SÍMBOLO DE LEGENDRE (a/b) É DEFINIDO COMO:
 (a/b)= { +1 SE a ≠ 0 mod(b) E PARA ALGUM x SE VERIFICA A RELAÇÃO a = x² mod(b) 
        { -1 SE a ≠ 0 mod(b) E PARA NENHUM x SE VERIFICA A RELAÇÃO a = x² mod(b) 
  
-OS SÍMBOLOS DE JACOBI SÃO CALCULADOS MULTIPLICANDO-SE OS REPECTIVOS SÍMBOLOS DE LEGENDRE DOS FATORES PRIMOS UM DADO NÚMERO ELEVADO AO SEU NÚMERO DE OCORRÊNCIA DE CADA FATOR NESTE NÚMERO.
-PARA NÚMEROS PRIMOS AMBOS DOS SÍMBOLOS SÃO IGUAIS.
+OS SÍMBOLOS DE JACOBI SÃO CALCULADOS MULTIPLICANDO-SE OS REPECTIVOS SÍMBOLOS DE LEGENDRE DOS FATORES PRIMOS UM DADO NÚMERO ELEVADO AO SEU NÚMERO DE OCORRÊNCIA DE CADA FATOR NESTE NÚMERO. PARA NÚMEROS PRIMOS AMBOS DOS SÍMBOLOS SÃO IGUAIS.
 
-UMA SÉRIE DE  RELAÇÕES RECURSIVAS PERMITE OBTER O SÍMBOLO DE JACOBI DE FORMA SIMPLES.
-PARA MAIORES INFORMAÇÕES: https://en.wikipedia.org/wiki/Jacobi_symbol
-
-
+UMA SÉRIE DE  RELAÇÕES RECURSIVAS PERMITE OBTER O SÍMBOLO DE JACOBI DE FORMA SIMPLES. UMA GENERALIZAÇÃO SIMPLES EXTENDE A DEFINIÇÃO PARA O CASO EM QUE a<0.
 O SÍMBOLO DE JACOBI É UM CASO ESPECIAL CARACTÉRE DE DIRICHLET A MENOS DE UMA RELAÇÃO DE CONGRUÊNCIA.
-PARA MAIORES INFORMAÇÕES: https://en.wikipedia.org/wiki/Dirichlet_character
+
+PARA MAIORES INFORMAÇÕES: https://en.wikipedia.org/wiki/Jacobi_symbol
+                          https://en.wikipedia.org/wiki/Dirichlet_character
 
 */
 
@@ -27,8 +25,9 @@ PARA MAIORES INFORMAÇÕES: https://en.wikipedia.org/wiki/Dirichlet_character
 #include<assert.h>
 
 //Protótipo de funções
-int jacobi(int64_t, int64_t);
 int64_t euclides_check(int64_t, int64_t);
+int jacobi(int64_t, int64_t);
+int jacobi_symbol_extension(int64_t, int64_t);
 
 //********************************************************************************************************************
 //Função que implementa o algoritmo de Euclides para se calcular o mdc de dois inteiros
@@ -82,6 +81,41 @@ return j;
            };
                                   }; 
 
+//Função que extende o simbolo de jacobi para números negativos (a<0)
+int jacobi_symbol_extension(int64_t a, int64_t n){
+//Restrição
+assert(a<0);
+
+//Variáveis locais
+int64_t positive_a=(-1)*a;
+int64_t power2=0;
+int result;
+
+//Procedimentos
+//Determinando a paridade do numerador
+while((positive_a%2)==0){
+positive_a/=2;
+power2++;
+                        };
+
+//Definindo o símbolo de jacobi para |a|>0
+result=jacobi(positive_a, n);
+
+//Determinando o sinal do resultado
+//Caso 1: |a| é ímpar
+if(power2>0 && (power2%2)==1)
+result=(-1)*jacobi(positive_a, n);
+//Caso 2: |a| é par
+if(power2>0 && (power2%2)==0)
+return result;
+
+//Caso 2: |a| não contém fatores 2
+if(power2==0)
+result=(-1)*jacobi(positive_a, n);
+
+//Resultado
+return result;
+                                                 };
 
 //********************************************************************************************************************
 //FIM DO HEADER
