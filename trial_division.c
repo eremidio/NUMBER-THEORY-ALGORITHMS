7bcde07ad77f0f8167ceca4c6b1324963b182b8f
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-04-08 21:20:41 -0300

    Update trial_division.c

--- trial_division.c
+++ trial_division.c
@@ -1,64 +1,12 @@
-//VAMOS CRIAR UM PROGRAMA QUE TESTA A PRIMALIDADE DE UM INTEIRO DE 64 BITS USANDO TENTATIVA POR DIVISÃO
+//VAMOS CRIAR UM PROGRAMA QUE TESTAS AS ROTINAS CRIADAS NO ARQUIVO trial_division.h
 //COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o  trial_division trial_division.c -lm -O2
 
-//****************************************************************************************************************************
-//CABEÇALHO
-#include<stdio.h>
-#include<stdint.h>
-#include<stdbool.h>
-#include<math.h>
 
-//****************************************************************************************************************************
-//DECLARAÇÃO DE FUNÇÕES
-bool trial_division(uint64_t);
-
-//****************************************************************************************************************************
-//FUNÇÕES
-//Função que testa a primalidade de um número usaremos uma otimização 30k+1
-bool trial_division(uint64_t n){
-//Casos triviais primos menos que 30
-if(n==2 || n==3 || n==5 || n==7 || n==11 || n==13 || n==17 || n==23 || n==29)
-return true;
-
-//Variáveis locais
-uint64_t i;
-uint64_t limit = sqrt(n);
-uint64_t divisors[8]={1, 7, 11, 13, 17, 19, 23, 29};//Números coprimos com 30 no intervalo 1,...,30
-
-//Procedimentos
-//Teste de divisão por 2, 3, 5
-if((n%5)==0 || (n%3)==0 || (n%2)==0)
-return false;
-
-//Loop principal
-//1º iteração
-for(i=1; i<8; ++i){
-if((n%divisors[i])==0)
-return false;
-                  };
-
-//Demais interações
-while(divisors[7]<limit){
-//Atualizando variáveis
-for(i=0; i<8; ++i)
-divisors[i]=divisors[i]+30;
-
-//Testando possíveis divisores
-for(i=0; i<8; ++i){
-if((n%divisors[i])==0)
-return false;
-                  };
-
-                       };
-
-//Caso o número passe nos testes acima
-return true;
-
-                               };
+//Cabeçalho
+#include"trial_division.h"
 
 
-
-//FUNÇÃO PRINCIPAL
+//Função principal
 int main(){
 //Váriaveis locais
 uint64_t number;
@@ -66,13 +14,13 @@ uint64_t number;
 //Procedimentos
 //Recebendo input do usuário
 printf("Digite um número que será testado:\n");
-if(scanf("%lu", &number)!=1);
+if(scanf("%lu", &number)!=1)
+return 0;
 
 //Executando o teste
-if(trial_division(number)==true)
+if(trial_division(number, Yes)==true)
 printf("Primo encontrado!\n");
-else
-printf("Número composto.\n");
+
 
 //Finalizando a aplicação
 return 0;
