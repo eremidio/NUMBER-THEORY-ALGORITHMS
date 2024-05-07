e5d61295c18ac3d2362c8fa1d3f5cbd3181cb5a1
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-05-07 15:25:14 -0300

    Update mobius_function.h
    optimization mobius can now be calculated upto 10^19.
    Mertens functions is fast up 10^8

--- mobius_function.h
+++ mobius_function.h
@@ -10,18 +10,15 @@
 A FUNÇÃO DE MÖBIUS É MULTIPLICATIVA SE m E n SÃO RELATIVAMENTE PRIMOS ENTRE SI: μ(nm) = μ(n).μ(m)
 
 A APLICAÇÃO MAIS IMPORTANTE DA FUNÇÃO DE MÖBIUS É A CHAMADA FÓRMULA DE INVERSÃO:
-SEJAM f E g DUAS FUNÇÕES ARITMÉTICA TAIS QUE 
-g(n)= Σf(d), ONDE n É UM INTEIRO QUALQUER E d REPRESENTA O CONJUNTO DOS DIVISORES DE n
-ENTÃO VALE A RELAÇÃO:
-f(n)= Σμ(d)g(n/d)
+SEJAM f E g DUAS FUNÇÕES ARITMÉTICA TAIS QUE g(n)= Σf(d), ONDE n É UM INTEIRO QUALQUER E d REPRESENTA O CONJUNTO DOS DIVISORES DE n
+ENTÃO VALE A RELAÇÃO: f(n)= Σμ(d)g(n/d).
 
 A FÓRMULA DE INVERSÃO É UM CASO PARTICULAR DE UMA CONVOLUÇÃO DE DIRICHLET.
-
-ALGUMAS GENERALIZAÇÕES DA FUNÇÃO DE MÖBIUS SÃO TAMBṔEM DENOTADAS POR μ(n).
+ALGUMAS GENERALIZAÇÕES DA FUNÇÃO DE MÖBIUS SÃO TAMBÉM DENOTADAS POR μ(n).
 
 PARA MAIORES INFORMAÇÕES: https://en.wikipedia.org/wiki/Möbius_inversion_formula
-https://en.wikipedia.org/wiki/Möbius_function#cite_note-FOOTNOTEBostConnes1995411–457-6
-https://en.wikipedia.org/wiki/Dirichlet_convolution
+                          https://en.wikipedia.org/wiki/Möbius_function#cite_note-FOOTNOTEBostConnes1995411–457-6
+                          https://en.wikipedia.org/wiki/Dirichlet_convolution
 
 */
 
@@ -38,155 +35,249 @@ const unsigned int MOBIUS_BOUND = 100000;
 
 //*************************************************************************************************************************
 //DECLARAÇÃO DE FUNÇÕES
-bool is_prime(uint64_t);
-bool is_square(uint64_t);
+bool is_prime_mob(uint64_t);
+bool is_square_mob(uint64_t);
 int mobius_small_integer(uint64_t);
 int mobius_large_integer(uint64_t);
 int mobius_function(uint64_t);
 int64_t mertens_function(uint64_t);
-uint64_t euclides_check(uint64_t, uint64_t);
+uint64_t gcd_mob(uint64_t, uint64_t);
 
 //*************************************************************************************************************************
 //FUNÇÕES
 //Função que calcula se um número é primo ou não (usaremos uma otimização 6k+1)
-bool is_prime(uint64_t n){
-//Casos triviais
-//0,1
-if(n<=1)
-return false;
+bool is_prime_mob(uint64_t n){
 
-//Casos básicos
-if(n==2 || n==3)
-return true;
-if((n%2)==0 || (n%3)==0)
-return false;
+ //Casos triviais
+  //0,1
+  if(n<=1)
+   return false;
 
+  //Primos menores que 100
+  uint64_t prime_seed[25]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
+  
+  for(int p=1; p<25; ++p){
 
-//Loop principal
-for(uint64_t i=5; i<sqrt(n); i+=6){
-if((n%i)==0 || (n%(i+2))==0)
-return false;
-                                  };
-
-//Caso fatores primos não sejam encontrados
-return true;
+    if(n==prime_seed[p])
+     return true;
+    if((n%prime_seed[p])==0)
+     return false;
 
                          };
 
+  
+ //Caso geral:
+  //Variáveis locais
+  uint64_t sqrt_n =sqrt(n);
+
+  //Procedimento: loop sobre possíveis divisores usando uma otimização 30k+1
+
+  for(uint64_t i=30; i<(sqrt_n+1); i+=30){
+
+    if((n%(i+1))==0 || (n%(i+7))==0 || (n%(i+11))==0 || (n%(i+13))==0 || (n%(i+17))==0 || (n%(i+19))==0 || (n%(i+23))==0 || (n%(i+29))==0)
+     return false;
+
+                                        };
+
+  return true;
+
+                            };
+
 //Função que calcula de um número é um quadrado perfeito
-bool is_square(uint64_t n){
-//Procedimentos
-for(uint64_t i=10000; i<n; ++i){
-if(i*i>n)
-return false;
-                               };
-return true;
-                          };
+bool is_square_mob(uint64_t n){
+
+  return floor(sqrt(n))-sqrt(n)>=0;
+
+                              };
 
 //Função de Möbius para pequenos valores
 int mobius_small_integer(uint64_t n){
-//Caso trivial 
-if(n==1)
-return 1;
 
-//Variáveis locais
-uint64_t prime_factor_number=0;
-int result = 1;
-uint64_t n1=n;
+ //Caso trivial 
+ if(n==1)
+  return 1;
 
-//Procedimentos
-//Testando a divisibilidade por 2
-if((n%2)==0){
-n1/=2;
-if(n1==1)
-return (-1)*result;
-prime_factor_number++;
-if((n1%2)==0)
-return 0;
-            };
 
-//Testando a divisibilidade por números primos ímpares
-for(uint64_t i=3; n1>1; i+=2){
-if((n1%i)==0){
-n1/=i;
-prime_factor_number++;
-if(n1==1)
-break;
-if((n1%i)==0)
-return 0;
-            };
-                             };
-//Resultado
-if((prime_factor_number%2)==0)
-return result;
-if((prime_factor_number%2)==1)
-return (-1)*result;
+ //Variáveis locais
+ uint64_t prime_factor_number=0;
+ uint64_t root=sqrt(n);
+ int result = 1;
 
-                                    };
+
+ //Procedimentos
+  //Testando a divisibilidade por 2 e 3
+   if((n%2)==0){
+
+    n/=2;
+    prime_factor_number++;
+
+    if(n==1)
+     goto end;
+  
+
+    if((n%2)==0)
+     return 0;
+
+              };
+
+
+   if((n%3)==0){
+
+    n/=3;
+    prime_factor_number++;
+
+    if(n==1)
+     goto end;
+
+    if((n%3)==0)
+     return 0;
+
+              };
+
+
+   //Testando a divisibilidade por números primos ímpares até n^(1/2)
+   for(uint64_t i=5; n<(root+1); i+=6){
+
+    if((n%i)==0){
+
+     n/=i;
+     prime_factor_number++;
+
+    if(n==1)
+     goto end;
+
+    if((n%i)==0)
+     return 0;
+
+               };
+
+
+    if((n%(i+2))==0){
+
+     n/=(i+2);
+     prime_factor_number++;
+
+    if(n==1)
+     goto end;
+
+    if((n%(i+2))==0)
+     return 0;
+
+                   };
+
+
+                                      };
+
+   //Fator primo superior a n^(1/2)
+   if(n>1)
+    prime_factor_number++;
+
+
+ //Resultado
+ end:
+  if((prime_factor_number%2)==0)
+   return result;
+  if((prime_factor_number%2)==1)
+   return (-1)*result;
+
+                              };
 
 //Função que implementa o algoritmo de Euclides para se calcular o mdc de dois inteiros
-uint64_t euclides_check(uint64_t a, uint64_t b){
-if(b==0)
-return a;
-else
-return euclides_check(b, a%b);
-                                               };
+uint64_t gcd_mob(uint64_t a, uint64_t b){
+
+ if(b==0)
+  return a;
+ else
+  return gcd_mob(b, a%b);
+                                        };
+
+
 
 //Função de Möbius para grandes valores
 int mobius_large_integer(uint64_t n){
-//Variáveis locais
-int result = 1;
-//Caso 1: quadrados perfeitos
-if(is_square(n)==true)
-return 0;
-//Caso2 : números primos
-if(is_prime(n)==true)
-return (-1)*result;
 
-//Caso 3: explorando a propriedade multiplicativa da função de Möbius 
-//Variáveis
-uint64_t factor1, factor2;
-uint64_t sqrt_n=sqrt(n);
+ //Variáveis locais
+ int result = 1;
 
-//Procedimentos
-//Loop principal encontrando fatores relativamente primos entre si
-for(uint64_t i=sqrt_n; i>1; --i){
-if(n%i==0){
-factor1=i;
-factor2=n/i;
-          };
+  //Procedimentos
+   //Caso 1: quadrados perfeitos
+   if(is_square_mob(n)==true)
+    return 0;
 
-if(euclides_check(factor1, factor2)==1)
-break;
-                                };
 
-//Resultado
-return(mobius_small_integer(factor1)*mobius_small_integer(factor2));
+   //Caso2 : números primos
+   if(is_prime_mob(n)==true)
+    return (-1)*result;
+
+
+
+   //Caso 3: explorando a propriedade multiplicativa da função de Möbius 
+    //Variáveis
+    uint64_t factor1, factor2;
+    uint64_t sqrt_n=sqrt(n);
+
+
+    //Procedimentos
+     //Loop principal encontrando fatores relativamente primos entre si
+    for(uint64_t i=(sqrt_n-1); i>1; --i){
+
+     if((n%i)==0){
+      factor1=i;
+      factor2=n/i;
+
+      if(gcd_mob(factor1, factor2)==1)
+       break;
+                };
+
+
+     if((n%(sqrt_n-i))==0 && (sqrt_n-i)>=2){
+      factor1=(sqrt_n-i);
+      factor2=n/(sqrt_n-i);
+
+      if(gcd_mob(factor1, factor2)==1)
+       break;
+                                           };
+
+
+
+                                        };
+
+ //Resultado
+ if(factor1>MOBIUS_BOUND && factor2<=MOBIUS_BOUND)
+  return (mobius_large_integer(factor1)*mobius_small_integer(factor2));
+ if(factor1<=MOBIUS_BOUND && factor2>MOBIUS_BOUND)
+  return (mobius_small_integer(factor1)*mobius_large_integer(factor2));
+ else
+  return(mobius_small_integer(factor1)*mobius_small_integer(factor2));
 
                                     };
 
+
 //Função de Möbius
 int mobius_function(uint64_t n){
-if(n<=MOBIUS_BOUND)
-return mobius_small_integer(n);
-else
-return mobius_large_integer(n);
+
+ if(n<=MOBIUS_BOUND)
+  return mobius_small_integer(n);
+ else
+  return mobius_large_integer(n);
 
                                };
 
 
 //*************************************************************************************************************************
 //APLICAÇÃO NO CÁLCULO DA FUNÇÃO DE MERTENS
+
 int64_t mertens_function(uint64_t n){
-//Variáveis locais
-int64_t result=0;
 
-//Procedimento
-for(uint64_t i=1; i<=n; ++i)
-result+= mobius_function(i);
+ //Variáveis locais
+ int64_t result=0;
 
-//Resultado
-return result;
+ //Procedimento:loop principal
+ for(uint64_t i=1; i<=n; ++i)
+  result+= mobius_function(i);
+
+ //Resultado
+ return result;
                                     };
 
 //*************************************************************************************************************************
