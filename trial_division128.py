c24d865e4f63efda0c28985306094d1344df3717
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-05-13 00:38:09 -0300

    Update trial_division128.py
    Add Miller-Rabin primality test to stop the algorithm earlier avoiding unnecessary divisions. It work fine for most numbers up to 10^30, only for semiprimes above to 10^20 the running time is slow

--- trial_division128.py
+++ trial_division128.py
@@ -1,5 +1,4 @@
-#VAMOS CRIAR UM PROGRAMA  QUE IMPLMENTA  O ALGORITMO DE DIVISÃO POR TENTATIVA E ERRO PARA CALCULA FATORES PRIMOS DE UM INTEIRO DE 128
-
+#VAMOS CRIAR UM PROGRAMA  QUE IMPLMENTA  O ALGORITMO DE DIVISÃO POR TENTATIVA E ERRO PARA CALCULA FATORES PRIMOS DE UM INTEIRO DE 128 BITS
 
 
 #IMPORTANDO BIBLIOTECAS
@@ -7,7 +6,62 @@ from math import sqrt
 
  
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-#FUNÇÕES PROGRAMA PRINCIPAL
+#FUNÇÕES 
+
+def miller_rabin128(n:int)->bool:
+ '''Função que implementa o teste de Miller-Rabin para inteiros até 10^25, será usado como subrotina no algoritmo principal'''
+
+ #Variáveis locais
+ prime_seed:int = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+ n_even:int = (n-1)
+ s:int = 0
+ d:int=0
+ is_prime = False
+
+ #Procedimentos
+ #Caso base:
+ if(n<2):
+  return False
+ 
+ #Testes com primos inferiores a 100
+ for prime in prime_seed:
+  if(n == prime):
+   return True
+  if((n%prime)==0):
+   return False
+
+
+
+ #Cálculo dos parâmetros s e d usados no algoritmo  
+ while((n_even%2) == 0):
+  n_even //= 2
+  s += 1
+ d = n_even
+
+
+ #Loop principal usando a base de primos 
+ for a in prime_seed:
+  if(a>=n):
+   break
+
+  x:int =pow(a, d, n)
+  if(x == 1 or x == n - 1):
+   continue 
+
+  for i in range(s - 1):
+
+   x = pow(x, 2, n)
+   if(x == n-1):
+    is_prime = True
+    break
+
+  if(is_prime==False):
+   return False
+
+ return True
+
+
+
 
 def trial_division(n:int)->list:
     '''Função que implementa o algoritmo da tentativa de divisão para fatorar números inteiros'''
@@ -26,6 +80,10 @@ def trial_division(n:int)->list:
           while((n%small_primes)==0):
               n//=small_primes
 
+    if(miller_rabin128(n)==True):
+        prime_factors.append(n)
+        return prime_factors
+
     #Loop principal com otimização 210k+r
     for x in range(n):
 
@@ -39,6 +97,10 @@ def trial_division(n:int)->list:
                 prime_factors.append(divisor)
                 while((n%divisor)==0):
                     n//=divisor
+                if(miller_rabin128(n)==True):
+                    prime_factors.append(n)
+                    return prime_factors
+
                     
     #Fatores superiores a n^(1/2)
     if(n>1):
@@ -62,4 +124,3 @@ print('Eis os fatores primos de ', n, '=', prime_factor_list)
 
 if(len(prime_factor_list)==1):
     print(n, 'é primo.')
-
