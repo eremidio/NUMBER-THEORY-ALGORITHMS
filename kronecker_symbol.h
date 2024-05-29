d1105a3f6130154f25f0c6b8039b885fd1ff23f3
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-05-29 01:59:03 -0300

    Update kronecker_symbol.h
    FASTER CALCULATION USING QUADRATIC RECIPROCITY

--- kronecker_symbol.h
+++ kronecker_symbol.h
@@ -1,136 +1,167 @@
-//VAMOS CRIAR UM PROGRAMA QUE COMPUTA OS CHAMADOS SÍMBOLOS DE KRONECKER PARA INTEIROS
+// VAMOS CRIAR UM PROGRAMA QUE COMPUTA OS CHAMADOS SÍMBOLOS DE KRONECKER PARA INTEIROS
 
 /*
-OS SÍMBOLOS DE KRONECKER SÃO GENERALIZAÇÕES (EXTENSÕES) DOS SÍMBOLOS DE LEGENDRE VÁLIDOS PARA TODOS OS NÚMEROS INTEIROS.
+OS SÍMBOLOS DE KRONECKER SÃO GENERALIZAÇÕES (EXTENSÕES) DOS SÍMBOLOS DE LEGENDRE
+E DE JACOBI VÁLIDOS PARA TODOS OS NÚMEROS INTEIROS.
+
 PARA MAIORES INFORMAÇÕES: https://en.wikipedia.org/wiki/Kronecker_symbol
+                          Elementary Number Theory by Edmund Landau
+                          A Course In Computational Algebraic Number Theory by Henri Cohen
 
 */
 
 //*****************************************************************************************************************************************************************
-//CABEÇALHO
+// CABEÇALHO
 #ifndef KRONECKER_SYMBOL_H
 #define KRONECKER_SYMBOL_H
-#include"legendre_symbol.h"//Dependências necessárias estão inclusas
+#include<stdbool.h>
+#include<stdint.h>
+#include<inttypes.h>
 #include<stdio.h>
+#include<assert.h>
 
 //*****************************************************************************************************************************************************************
-//DECLARAÇÃO DE FUNÇÕES
-bool quick_prime_checking(int64_t);
+// DECLARAÇÃO DE FUNÇÕES
+
 int kronecker(int64_t, int64_t);
 
 //*****************************************************************************************************************************************************************
-//FUNÇÕES
-//Função que testa se um número é primo
-bool quick_prime_checking(int64_t n){
-//Casos triviais
-if(n<2)
-return false;
-if(n==2 || n==3)
-return true;
-if((n%2)==0 || (n%3)==0)
-return false;
+// FUNÇÕES
 
-//Variáveis locais
-uint64_t i, limit=sqrt(n);
-
-//Procedimentos
-//Loop principal com otimização 6k+1
-for(i=5; i<limit; i+=6){
-if((n%i)==0 || (n%(i+2))==0)
-return false;
-                       };
-
-return true;
-                                    };
-
-//Função que calcula o símbolo de Kronecker (a|n)
+// Função que calcula o símbolo de Kronecker (a|n)
 int kronecker(int64_t a, int64_t n){
-//Variáveis locais
-int result=1;
 
-//Casos triviais
-//Números não relativamente primos
-if(euclides_algorithm(a, n)>1)
-return 0;
+  //Restrição
+  assert(n*(-1)==1 || n>=0);
 
-//n=2
-if(n==2){
-if((a%8)==1 || (a%8)==7)
-return result;
-if((a%8)==3 || (a%8)==5)
-return (-1)*result;
-        };
+  //Variável global:resultado
+  int k=1;
 
-//n=0, 1, -1
-if(n==0){
-if((a*a)==1)
-return result;
-else
-return 0;
-        };
-
-if(n==1)
-return result;
-
-if(n==(-1)){
-if(a<0)
-return (-1)*result;
-else
-return result;
+  //Caso base: n= 2
+  if(n==2){
+    if((a%8)==1 || (a%8)==7)
+      return k;
+    if((a%8)==3 || (a%8)==5)
+      return (-1)*k;
+    else
+      return 0;
            };
 
-//n é primo
-if(a>0 && n>0 && quick_prime_checking(n)==true)
-return legendre((uint64_t)a, (uint64_t)n);
+  //Caso base: n=0
+  if(n==0){
+    if((a*a)==1)
+      return k;
+    else
+      return 0;
 
-//Caso ambos sejam negativos
-if(a<0 && n<0)
-return (-1)*kronecker((-1)*a, (-1)*n);
+          };
 
-//Caso a ou n negativo
-if(a<0 && n>=0)
-return kronecker((-1)*a, n);
-if(a>=0 && n<0)
-return kronecker(a, (-1)*n);
+  //Caso base: n=1
+  if(n==1)
+    return k;
 
-//Caso geral n é composto
-if(quick_prime_checking(n)==false){
-//Variáveis locais
-int64_t n_=n;
-int64_t i;
-int64_t limit=sqrt(n);
-int result=1;
-
-//Procedimentos
-
-//Fatores 2
-while((n_%2)==0){
-result*=kronecker(a, 2);
-n_/=2;
-                };
-
-//Fatores ímpares
-for(i=3; i<=limit; i+=2){
-while((n_%i)==0){
-result*=kronecker(a, i);
-n_/=i;
-                };
-
-                            };
-
-//Resultado
-if(n_<=1)
-return result;
-
-//Caso o número em questão tenha um fator primo maior que n^(1/2)
-if(n_>1)
-return result*legendre(a, n_);
-
-                                  };
+  //Caso base: n= -1
+  if((-1)*n==1){
+    if(a>=0)
+      return k;
+    else
+      return (-1)*k;
+             };
 
 
-                                       };
+  //Caso trivial: a, n são ambos pares 
+  if(!(a&1) && !(n&1))
+    return 0;
+
+  //Caso geral: usando a lei da reciprocidade quadrática para computar o símbolo de Kronecker de forma eficiente
+    //Variáveis locais
+    int residue_table[8]={0,1,0, (-1), 0, (-1), 0, 1};
+    int64_t b=n;
+    int64_t r=0, v=0;
+
+
+    //Procedimentos
+
+     //TESTE USE /**/ APÓS O MESMO
+     /*printf("a:%lu b:%lu\n", a, b);
+     for(int x=0; x<8; x++)
+      printf("table_entry:%i ", residue_table[x]);
+     printf("\n");*/
+
+
+      step2:
+      //Removendo fatores 2 do segundo argumento
+      v=0;
+      while(!(b&1)){
+        v++;
+        b>>=1;
+                   };
+
+      //Ajuste de variáveis
+      if(!(v&1))
+        k=1;
+      else
+        k=residue_table[a&7];
+
+    
+      if(a<0) k=(-1)*k;
+
+
+     //TESTE USE /**/ APÓS O MESMO
+     /*printf("a:%lu b:%lu v:%lu k:%d\n", a, b, v, k);*/
+
+
+       step3:
+       //Reduzindo o valor de a (mod b)
+       a%=b;
+
+       step4:
+      //Condição que determina o fim do algoritmo
+      if(a==0 && b>1)
+        return 0;
+      if(a==0 && b==1)
+        return k;
+
+ 
+      step5:
+      //Ajuste de variáveis
+      v=0;
+      while(!(a&1)){
+        v++;
+        a>>=1;
+                   };
+
+      if((v&1))
+       k=residue_table[b&7];
+
+     
+      step6:
+      r=b-a;
+      if(r>=0){
+
+       //Aplicando a lei da reciprocidade quadrática
+       if(a&b&2)
+        k=(-1)*k;
+
+       //Ajuste de variáveis
+       b=a;
+       a=r;
+               };
+     
+       if(r<0) a=(-1)*r;
+
+
+
+     //TESTE USE /**/ APÓS O MESMO
+     /*printf("a:%lu b:%lu v:%lu r:%lu\n", a, b, v, r);*/
+
+     goto step4;
+
+
+    //Em caso de erro 
+    return 2;
+                                   };
 
 //*****************************************************************************************************************************************************************
-//FIM DO HEADER
+// FIM DO HEADER
 #endif
