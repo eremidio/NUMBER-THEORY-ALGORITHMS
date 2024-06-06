f766293b2801c42642fcd6aa44e96602fb6d7d7c
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-06-06 17:16:49 -0300

    Update euler_totient_function.h
    more robust implementation for large odd powers of 10 returns half the correct value.

--- euler_totient_function.h
+++ euler_totient_function.h
@@ -1,164 +1,246 @@
-//VAMOS ESCREVER UM PROGRAMA PARA COMPUTAR A FUNÇÃO TOTIENTE DE EULER TAMBÉM CONHECIDA COMO FUNÇÃO φ DE EULER
+// VAMOS ESCREVER UM PROGRAMA PARA COMPUTAR A FUNÇÃO TOTIENTE DE EULER TAMBÉM
+// CONHECIDA COMO FUNÇÃO φ DE EULER
 
 /*
-PARA UM ARGUMENTO n A FUNÇÃO φ(n) CONTABALIZA QUANTOS INTEIROS NO INTERVALO [1, 2, ..., n] SÃO RELATIVAMENTE PRIMOS COM n.
-ISTO É, DADO UM NÚMERO a NO INTERVALO 1, 2,..., n, φ(n) CONTABILIZA QUANTOS NÚMEROS SATISFAZEM A CONDIÇÃO mdc(a, n)=1.
+PARA UM ARGUMENTO n A FUNÇÃO φ(n) CONTABALIZA QUANTOS INTEIROS NO INTERVALO [1,
+2, ..., n] SÃO RELATIVAMENTE PRIMOS COM n. ISTO É, DADO UM NÚMERO a NO INTERVALO
+1, 2,..., n, φ(n) CONTABILIZA QUANTOS NÚMEROS SATISFAZEM A CONDIÇÃO mdc(a, n)=1.
 
-A FUNÇÃO φ(n) É MULTIPLICATIVA: φ(n)φ(m)=φ(nm) SE n E m FOREM  RELATIVAMENTE PRIMOS.
-PARA UM NÚMERO PRIMO p: φ(p)=p-1 (APENAS 1 É RELATIVAMENTE PRIMO COM n).
+A FUNÇÃO φ(n) É MULTIPLICATIVA: φ(n)φ(m)=φ(nm) SE n E m FOREM  RELATIVAMENTE
+PRIMOS. PARA UM NÚMERO PRIMO p: φ(p)=p-1 (APENAS 1 É RELATIVAMENTE PRIMO COM n).
 
-ESTA FUNÇÃO PODE SER DETERMINADA POR MEIO DE CONTAGEM DIRETA USANDO O ALGORITMO DE EUCLIDES OU USANDO-SE A FÓRMULA DE EULER, QUE FAZ USO DE UM PRODUTÓRIO ENVOLVENDO FATORES PRIMOS DE n.
+ESTA FUNÇÃO PODE SER DETERMINADA POR MEIO DE CONTAGEM DIRETA USANDO O ALGORITMO
+DE EUCLIDES OU USANDO-SE A FÓRMULA DE EULER, QUE FAZ USO DE UM PRODUTÓRIO
+ENVOLVENDO FATORES PRIMOS DE n.
 
-PARA MAIORES INFORMAÇÕES:https://en.wikipedia.org/wiki/Euler%27s_totient_function
+PARA MAIORES
+INFORMAÇÕES:https://en.wikipedia.org/wiki/Euler%27s_totient_function
 
 */
 
 //*************************************************************************************************************************
-//CABEÇALHO
+// CABEÇALHO
 #ifndef EULER_TOTIENT_FUNCTION_H
 #define EULER_TOTIENT_FUNCTION_H
-#include<stdlib.h>
-#include<stdbool.h>
-#include<stdint.h>
-#include<math.h>
+#include"gauss_euler_primality_test.h"
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-#define PHI_MAX 500000
+#define PHI_MAX 250000
 
-//Limite máximo: 9223372036854775807
 
 //*************************************************************************************************************************
-//FUNÇÕES
+// FUNÇÕES
 
-//Função que implementa o algoritmo de Euclides para se calcular o mdc de dois inteiros
-uint64_t gcd_euclides(uint64_t a, uint64_t b){
-if(b==0)
-return a;
-else
-return gcd_euclides(b, a%b);
-                                             };
+// Função que implementa o algoritmo de Euclides para se calcular o mdc de dois
+// inteiros
+uint64_t gcd_euclides(uint64_t a, uint64_t b) {
+  if (b == 0)
+    return a;
+  else
+    return gcd_euclides(b, a % b);
+};
 
-//Função que detecta se um número é quadrado perfeito
-bool perfect_square(uint64_t n){
-return floor(sqrt(n))-sqrt(n)>=0;
-                               };
+// Função que detecta se um número é quadrado perfeito
+bool perfect_square(uint64_t n) { return floor(sqrt(n)) - sqrt(n) >= 0; };
 
-//Função que calcula se um número é primo ou não
-bool is_prime(uint64_t n){
+// Função que calcula um fator primo de um inteiro de 64 bits usando o algoritmo de Pollard-rho
+uint64_t pollard_rho(uint64_t n){
 
- //Casos triviais
-  //0,1
-  if(n<=1)
-   return false;
-
-  //Primos menores que 100
-  uint64_t prime_seed[25]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
-  
-  for(int p=1; p<25; ++p){
-
-    if(n==prime_seed[p])
-     return true;
-    if((n%prime_seed[p])==0)
-     return false;
-
-                         };
-
-  
- //Caso geral:
   //Variáveis locais
-  uint64_t sqrt_n =sqrt(n);
-
-  //Procedimento: loop sobre possíveis divisores usando uma otimização 30k+1
-
-  for(uint64_t i=30; i<(sqrt_n+1); i+=30){
-
-    if((n%(i+1))==0 || (n%(i+7))==0 || (n%(i+11))==0 || (n%(i+13))==0 || (n%(i+17))==0 || (n%(i+19))==0 || (n%(i+23))==0 || (n%(i+29))==0)
-     return false;
-
-                                        };
-
-  return true;
-                         };
+  uint64_t remainder=1;
+  uint64_t a=1, counter=0;
+  uint64_t x=2, y=2;
 
 
-//Função totiente para pequenos inteiros ≲250000
-uint64_t totient_function_small_numbers(uint64_t n){
+  //Procedimentos
+    //Testando inicialmente com x=2, y=2
+    restart:
+    while(remainder==1){
+      x=((x*x+1)%n);
+      y=((y*y+1)%n);
+      y=((y*y+1)%n);
 
- //Variáveis locais
- uint64_t counter=0;
- uint64_t i;//Variável de iteração
+      if(x>y)
+        remainder=gcd_euclides((x-y), n);
+      if(x<y)
+        remainder=gcd_euclides((y-x), n);
+
+      if(remainder>1 && remainder<n) return remainder;
 
 
- //Procedimentos: contagem de direta de números coprimos usando o algoritmo de Euclides
- for(i=1; i<n; ++i){
-  if(gcd_euclides(i, n)==1)
-  ++counter;
+    //Atualizando variáveis
+    counter++;
+    if(counter>1e6){
+      counter=0;
+      a++;
                   };
 
- return counter;
-                                                  };
 
+    };
 
-//Função totiente de Euler para números grandes (usamos a propriedade multiplicativa da função φ para ≲250000²)
-uint64_t totient_function_large_numbers(uint64_t n){
+  return 0;//Caso de falha do algoritmo
+                        
+};
+
+//Função que checa se um inteiro é da forma p^k com p primo e k inteiro
+bool power_prime_detection(uint64_t n, uint64_t* prime,  uint64_t* power){
 
 
  //Variáveis locais
- uint64_t factor1, factor2;
- uint64_t limit=sqrt(n);
- uint64_t i;//Variável de iteração
+ uint64_t tester=2;
+ uint64_t sqrt_n=sqrt(n); 
 
- //Procedimentos
-  //Caso 1: quadrados perfeitos
-  if(perfect_square(n)==true)
-   return totient_function_large_numbers(limit)*limit;
+  //Procedimentos 
+    //Caso particular números da forma 2^k e 3^k
+    while(tester<n){
+      (*power)=1;
 
-  //Caso 2: Números primos
-  if(is_prime(n))
-   return (n-1);
+      if(tester==n){
+        (*prime)=2;
+        return true;
+                }
+
+     tester<<=1;
+     (*power)++;
+
+    };
+
+    if(n%3==0){
+
+      (*power)=0;
+      while(n%3==0){
+        n/=3;
+        (*power)++;
+                   }
+
+      if(n==1){
+        (*prime)=3;
+        return true;
+              }
+      else
+        return false;
+
+    };
+
+
+    //Loop principal
+    for(uint64_t i=5; i<=sqrt_n; i+=6){
+
+      if(n%i==0){
+        (*power)=0;
+        while(n%i==0){
+          n/=i;
+          (*power)++;
+                     }
+
+          if(n==1){
+            (*prime)=i;
+            return true;
+                  }
+          else
+            return false;
+       };
+
+
+    if((n%(i+2))==0){
+
+        (*power)=0;
+        while(n%(i+2)==0){
+          n/=(i+2);
+          (*power)++;
+                         }
+
+      if(n==1){
+        (*prime)=(i+2);
+        return true;
+              }
+      else
+        return false;
+
+    };
  
-
-  //Caso 3: Encontrando fatores relativamente primos de um dado número
-  for(i=2; i<limit; ++i){
-
-   if(n%(limit-i)==0){
-
-    factor1=limit-i;
-    factor2=n/factor1;
-   if(gcd_euclides(factor1, factor2)==1)
-    break;
-
-                   };
-
-   if(n%i==0){
-    factor1=i;
-    factor2=n/factor1;
-   if(gcd_euclides(factor1, factor2)==1)
-    break;
-             };
-
-
-                      };
-
+                                  };
 
  //Resultado
- if(factor1>PHI_MAX && factor2<=PHI_MAX)
-  return totient_function_large_numbers(factor1)*totient_function_small_numbers(factor2);
- if(factor2>PHI_MAX && factor1<=PHI_MAX)
-  return totient_function_large_numbers(factor2)*totient_function_small_numbers(factor1);
- else
-  return totient_function_small_numbers(factor2)*totient_function_small_numbers(factor1);
-                                                   };
+ return false;
+  
 
+};
 
-//Função totiente de Euler
-uint64_t euler_totient_function(uint64_t n){
+// Função totiente para pequenos inteiros ≲250000
+uint64_t totient_function_small_numbers(uint64_t n) {
+  // Variáveis locais
+  uint64_t counter = 0;
+  uint64_t i;  // Variável de iteração
 
- if(n>PHI_MAX)
-  return totient_function_large_numbers(n);
- else
-  return totient_function_small_numbers(n);
-                                           };
+  // Procedimentos: contagem de direta de números coprimos usando o algoritmo de
+  // Euclides
+  for (i = 1; i < n; ++i) {
+    if (gcd_euclides(i, n) == 1) ++counter;
+  };
+
+  return counter;
+};
+
+// Função totiente de Euler para números grandes (usamos a propriedade
+// multiplicativa da função φ para ≲250000²)
+uint64_t totient_function_large_numbers(uint64_t n) {
+  // Variáveis locais
+  uint64_t factor1, factor2, common;
+  uint64_t limit = sqrt(n), prime, power;
+  uint64_t i;  // Variável de iteração
+
+  // Procedimentos
+  // Caso 1: quadrados perfeitos
+  if (perfect_square(n) == true)
+    return totient_function_large_numbers(limit) * limit;
+
+  // Caso 2: Números primos
+  if (gauss_euler_primality_test(n)) return (n - 1);
+
+  //Caso 3: potências de números primos
+  if (power_prime_detection(n, &prime, &power)) return ( (prime-1) * bin_pow(prime,(power - 1) ) );
+
+  // Caso 4: Encontrando fatores relativamente primos de um dado número
+  factoring:
+    factor1=pollard_rho(n);
+    factor2=n/factor1;
+
+    //Ajuste de fatores
+    common=gcd_euclides(factor1, factor2);
+    if(common>1){
+      factor2/=common;
+      factor1*=common;
+
+    }    
+    
+
+  // Resultado
+  if (factor1 > PHI_MAX && factor2 <= PHI_MAX)
+    return totient_function_large_numbers(factor1) *
+           totient_function_small_numbers(factor2);
+  if (factor2 > PHI_MAX && factor1 <= PHI_MAX)
+    return totient_function_large_numbers(factor2) *
+           totient_function_small_numbers(factor1);
+  if (factor2 < PHI_MAX && factor1 < PHI_MAX)
+    return totient_function_small_numbers(factor2) *
+           totient_function_small_numbers(factor1);
+  else
+    return totient_function_large_numbers(factor2) *
+           totient_function_large_numbers(factor1);
+};
+
+// Função totiente de Euler
+uint64_t euler_totient_function(uint64_t n) {
+  if (n > PHI_MAX)
+    return totient_function_large_numbers(n);
+  else
+    return totient_function_small_numbers(n);
+};
 
 //*************************************************************************************************************************
-//FIM DO HEADER
+// FIM DO HEADER
 #endif
