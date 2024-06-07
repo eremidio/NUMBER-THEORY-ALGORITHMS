2268c8f0dc15dd783c80b20d4aa1c3f7e6cc9fb5
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-06-07 03:41:19 -0300

    Update baby_step_giant_step.cpp

--- baby_step_giant_step.cpp
+++ baby_step_giant_step.cpp
@@ -1,31 +1,36 @@
 //VAMOS CRIAR UM PROGRAMA TESTAR AS ROTINAS CRIADAS NO ARQUIVO baby_step_giant_step.hpp
-//COMPILAR ESTE PROGRAMA COM O COMANDO: g++ -o baby_step_giant_step baby_step_giant_step.cpp
+//COMPILAR ESTE PROGRAMA COM O COMANDO: g++ -o  baby_step_giant_step baby_step_giant_step.cpp -O2
 
-
-//Cabeçalho
-#include"baby_step_giant_step.hpp"
-#include<iostream>
+// Cabeçalho
+#include "baby_step_giant_step.hpp"
+#include <iostream>
 using namespace std;
 
-//Função principal
+// Função principal
 int main(){
-//Variáveis locais 
-int64_t a, c, m, k;
-//Procedimento
-//Recebendo input do usuário
-cout<<"Digite um inteiro: ";
-cin>>a;
-cout<<"Digite um inteiro: ";
-cin>>c;
-cout<<"Digite um inteiro: ";
-cin>>m;
 
-//Calculando o logaritmo discreto
-k=discrete_logarithm(a, c, m);
-if(k==(-1))
-cout<<"Não existe logaritmo natural de "<< c<< " na base "<< a<<" módulo "<< m<<".\n";
-else
-cout<<"Logaritmo natural de "<< c<< " na base "<< a<<" módulo "<< m<<" é "<< k<<".\n";
-//Encerrando a aplicação
-return 0;
+  // Variáveis locais
+  int64_t a, c, m, k;
+
+
+  // Procedimento
+    // Recebendo input do usuário
+    cout << "Digite um inteiro (base): ";
+    cin >> a;
+    cout << "Digite um inteiro (logaritmando): ";
+    cin >> c;
+    cout << "Digite um inteiro(congruente): ";
+    cin >> m;
+
+    // Calculando o logaritmo discreto
+    k = baby_step_giant_step(a, c, m);
+    if (k == (-1))
+      cout<< "Não existe logaritmo natural de " << c << " na base " << a << " módulo " << m << ".\n";
+     else
+      cout<< "Logaritmo natural de " << c << " na base " << a << " módulo " << m << " é " << k << ".\n";
+
+
+  // Encerrando a aplicação
+  return 0;
+
           }
