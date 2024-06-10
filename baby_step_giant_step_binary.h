8119712edd32fb23905e48dffe4031b49a96aa34
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-06-10 12:54:27 -0300

    Update and rename baby_step_giant_step_linear.h to baby_step_giant_step_binary.h
    correct binary serach version of the algorithm

--- baby_step_giant_step_binary.h
+++ baby_step_giant_step_binary.h
@@ -1,5 +1,4 @@
-//VAMOS CRIAR UM PROGRAMA QUE IMPLEMENTA O ALGORITMO BABY-STEP-GIANT-STEP USANDO BUSCA LINEARES
-
+//VAMOS CRIAR UM PROGRAMA QUE IMPLEMENTA O ALGORITMO BABY-STEP-GIANT-STEP USANDO BUSCA BINÁRIA
 
 /*
 PARA MAIORES INFORMAÇÕES: https://github.com/eremidio/NUMBER-THEORY-ALGORITHMS/blame/main/baby_step_giant_step.hpp
@@ -8,8 +7,8 @@ PARA MAIORES INFORMAÇÕES: https://github.com/eremidio/NUMBER-THEORY-ALGORITHMS
 
 //********************************************************************************************************************************
 //CABEÇALHO
-#ifndef BABY_STEP_GIANT_STEP_LINEAR_H
-#define BABY_STEP_GIANT_STEP_LINEAR_H
+#ifndef BABY_STEP_GIANT_STEP_BINARY_H
+#define BABY_STEP_GIANT_STEP_BINARY_H
 #include"mod_bin_exponentiation128.h"
 #include<math.h>
 #include<stdint.h>
@@ -20,56 +19,49 @@ PARA MAIORES INFORMAÇÕES: https://github.com/eremidio/NUMBER-THEORY-ALGORITHMS
 
 //********************************************************************************************************************************
 //ESTRUTURA USADA NO ALGORITMO
-  struct log_map{
-
-  int64_t index;
-  int64_t value;
-
- };
-
+struct log_map{
+    int64_t index;
+    int64_t value;
+};
 
 //********************************************************************************************************************************
 //DECLARAÇÃO DE FUNÇÕES
 void swap(int64_t, int64_t, struct log_map[]);
 void quicksort_ascending(struct log_map[],int64_t, int64_t);
-int64_t baby_step_giant_step_linear(int64_t, int64_t, int64_t);
+int64_t binary_search(struct log_map[], int64_t, int64_t, int64_t);
 
 
 //********************************************************************************************************************************
 //FUNÇÕES
 //Função que troca dois elementos em um array
 void swap(int64_t i, int64_t j, struct log_map log_array[]){
+    //Variáveis locais
+    struct log_map temp;
 
-  //Variáveis locais
-  struct log_map temp;
-
-  //Procedimento
+    //Procedimento
     //Trocando os elementos do array
     temp=log_array[i];
     log_array[i]=log_array[j];
     log_array[j]=temp;
-
-};
-
+}
 
 //Função que implementa o algoritmo quicksort para ordenar um array de 64-bits em ordem crescente
 void quicksort_ascending(struct log_map log_array[], int64_t low, int64_t high){
+    //Caso base: o array contém apenas 1 elemento
+    if(low>=high) return;
 
-  //Caso base: o array contém apenas 1 elemento
-  if(low>=high) return;
+    //Variáveis locais
+    int64_t i, last;
 
-  //Variáveis locais
-  int64_t i, last;
-
-  //Procedimentos
+    //Procedimentos
     //Movendo o elemento da partição
     swap(low, ((low+high)/2), log_array);
     last=low;
 
     //Organizando uma subpartição da lista
     for(int64_t i=(low+1); i<=high; ++i){
-      if(log_array[i].value<log_array[low].value)
-        swap(++last, i, log_array);
+        if(log_array[i].value<log_array[low].value)
+            swap(++last, i, log_array);
     }
 
     //Retornando o elemento pivotal a sua posição original
@@ -78,67 +70,64 @@ void quicksort_ascending(struct log_map log_array[], int64_t low, int64_t high){
     //Execução recursiva do algoritmo: a lista é particionada em dois subarrays
     quicksort_ascending(log_array, low, (last-1));
     quicksort_ascending(log_array, (last+1), high);
-  
-};
+}
 
+//Função que implementa a busca binária em um array de struct log_map
+int64_t binary_search(struct log_map log_array[], int64_t low, int64_t high, int64_t key){
+
+    //Loop principal
+    while (low <= high){
+        int64_t mid = low + (high - low) / 2;
+        // Se o elemento estiver presente no meio
+        if (log_array[mid].value == key)
+            return mid;
+        // Se o elemento for maior, ignore a metade esquerda
+        if (log_array[mid].value < key)
+            low = mid + 1;
+        // Se o elemento for menor, ignore a metade direita
+        else
+            high = mid - 1;
+    }
+    // Se o elemento não estiver presente no array
+    return -1;
+}
 
 //Função que implementa o algoritmo de Baby-Step-Giant_Step para computar logaritmos discretos usando buscas binárias
-int64_t baby_step_giant_step_linear(int64_t a, int64_t c, int64_t m){
+int64_t baby_step_giant_step_binary(int64_t a, int64_t c, int64_t m){
+    //Variáveis locais
+    int64_t n=ceil(sqrt(m)); //n^(1/2))
+    int64_t i, j;
+    struct log_map* power_in=NULL;
+    int64_t array_size=0;
+    int64_t result=0;
 
-  //Variáveis locais
-  int64_t n=ceil(sqrt(m)); //n^(1/2))
-  int64_t i, j;
-  struct log_map* power_in=NULL;
-  int64_t array_size=0;
-  int64_t result=0;
-
-
-
-  //Procedimentos
+    //Procedimentos
     //Pré-computando valores de a^(in) (mod m) e colocando seus elementos em ordem crescente
     for(i=0; i<=n; ++i){
-
-      array_size++;
-      power_in = (struct log_map*)realloc(power_in, array_size*sizeof(struct log_map));
-
-      int64_t value1 = mod_bin_pow(a, (i*n), m);
-      power_in[array_size-1].value=value1;
-      power_in[array_size-1].index=i;
-
+        array_size++;
+        power_in = (struct log_map*)realloc(power_in, array_size*sizeof(struct log_map));
+        int64_t value1 = mod_bin_pow(a, (i*n), m);
+        power_in[array_size-1].value=value1;
+        power_in[array_size-1].index=i;
     }
 
     quicksort_ascending(power_in, 0, (array_size-1));
 
-
     //Loop principal buscando por valores que satisfazem a relação a^(in)= c(a^j) (mod m)
     for(j=0; j<n; ++j){
-
-      int64_t value2=c*mod_bin_pow(a, j, m)%m;
-      for(i=0; i<=(n/2); ++i){
-
-        if(value2==power_in[i].value){
-          result=power_in[i].index*n-j;
-          goto end;
+        int64_t value2=c*mod_bin_pow(a, j, m)%m;
+        int64_t index = binary_search(power_in, 0, array_size-1, value2);
+        if (index != -1){
+            result=power_in[index].index*n-j;
+            goto end;
         }
-
-        if(value2==power_in[n-i].value){
-          result=power_in[n-i].index*n-j;
-          goto end;
-        }
-      
-                         }
     }
 
- 
-
-
-  //Limpando o cachê de memória e retornando o resultado
-  end:
-  if(power_in) free(power_in);
-  return result;
-    
-
-};
+    //Limpando o cachê de memória e retornando o resultado
+    end:
+    if(power_in) free(power_in);
+    return result;
+}
 
 //********************************************************************************************************************************
 //FIM DO HEADER
