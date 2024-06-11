4e7ea293889d195289ed0898be96f02c0a9fdf5a
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-06-10 23:41:51 -0300

    Update pohlig_hellman_logarithm.c
    correction of algorithm the prime power function now produces some correct results, further improvcements needed in the genral algorithm

--- pohlig_hellman_logarithm.c
+++ pohlig_hellman_logarithm.c
@@ -1,5 +1,5 @@
 //VAMOS CRIAR UM PROGRAMA PARA TESTAR AS ROTINAS CRIADAS NO ARQUIVO pohlig_hellman_logarithm.h
-//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o pohlig_hellman pohlig_hellman_logarithm.c 
+//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o pohlig_hellman pohlig_hellman_logarithm.c -lm
 
 //Cabeçalho
 #include"pohlig_hellman_logarithm.h"
@@ -7,26 +7,35 @@
 
 //Função principal
 int main(){
-//Variáveis locais
-int64_t log1, log2, g, h, N, p, e;
-//Procedimentos
-//Recebendo input do usuário
-printf("Digite o valor da base do logaritmo (gerador do grupo g): ");
-scanf("%li", &g);
-printf("Digite o valor da base do logaritmando h: ");
-scanf("%li", &h);
-printf("Digite o valor do número primo correspondente a ordem do grupo cíclico G1: ");
-scanf("%li", &p);
-printf("Digite o valor do expoente correspondente a ordem do grupo cíclico G1: ");
-scanf("%li", &e);
 
 
-//Executando o algoritmo de Pollard ρ para o cálculo de logaritmos discretos
-log1= pohlig_hellman_logarithm_prime_order(g, h, p, e);
-printf("Logaritmo de %li (mod %li) na base %li = %li\n", h,bin_pow(p,e), g, log1);
+  //Variáveis locais
+  int64_t log1, log2, g, h, N, p, e;
 
-//Finalizando a aplicação
-return 0;
+
+  //Procedimentos
+    //Recebendo input do usuário
+    printf("Digite o valor da base do logaritmo (gerador do grupo g): ");
+    scanf("%li", &g);
+    printf("Digite o valor da base do logaritmando h: "); 
+    scanf("%li", &h);
+    printf("Digite o valor do número primo correspondente a ordem do grupo cíclico G1: ");
+    scanf("%li", &p);
+    printf("Digite o valor do expoente correspondente a ordem do grupo cíclico G1: ");
+    scanf("%li", &e);
+
+    printf("Digite um inteiro correspondente a ordem do grupo cíclico G: ");
+    scanf("%li", &N);
+
+    //Executando o algoritmo de Pohlig-Hellman para o cálculo de logaritmos discretos
+    log1= pohlig_hellman_logarithm_power_prime_order(g, h, p, e);
+    printf("Logaritmo de %li (mod %li) na base %li = %li\n", h, (int64_t)(bin_pow(p,e)), g, log1);
+    
+    log2=pohlig_hellman_logarithm_generalized(g, h, N);
+    printf("Logaritmo de %li (mod %li) na base %li = %li\n", h, N, g, log2);
+
+  //Finalizando a aplicação
+  return 0;
+
           }
-
 
