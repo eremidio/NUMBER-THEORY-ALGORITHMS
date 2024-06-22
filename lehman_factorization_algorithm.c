6eb9c318016098d7dec7bd9bf56e3b279cd5da13
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-06-22 02:41:21 -0300

    Update lehman_factorization_algorithm.c

--- lehman_factorization_algorithm.c
+++ lehman_factorization_algorithm.c
@@ -1,5 +1,5 @@
 //VAMOS CRIAR UM PROGRAMA QUE TESTA AS ROTINAS CRIADAS NO ARQUIVO lehman_factorization_algorithm.h
-//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o lehman_factorization_algorithm lehman_factorization_algorithm.c -lm
+//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o lehman_factorization_algorithm lehman_factorization_algorithm.c -lm -O2
 
 //Cabeçalho
 #include"lehman_factorization_algorithm.h"
@@ -15,11 +15,12 @@ int main(){
   //Procedimentos
     //Recebendo input do usuário
     printf("Digite um número que será testado:\n");
-    scanf("%lu", &number);
+    if(scanf("%lu", &number)!=1) return 0;
 
     //Executando o teste
     lehman_factorization_algorithm(number);
 
-//Finalizando a aplicação
-return 0;
-          }
+  //Finalizando a aplicação
+  return 0;
+
+}
