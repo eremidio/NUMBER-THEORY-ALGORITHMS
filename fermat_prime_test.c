e9cca1ace08bc922ff8f6fcd2f4cdeac44ed332d
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-06-24 13:03:03 -0300

    Update fermat_prime_test.c

--- fermat_prime_test.c
+++ fermat_prime_test.c
@@ -1,110 +1,37 @@
-//VAMOS CRIAR UM PROGRAMA QUE IMPLEMENTA O ALGORITMO DE FERMAT PARA TESTAR SE UM NÚMERO É PRIMOU NÃO
-//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o fermat_prime_test fermat_prime_test.c -lm
-
-/*
-
-O TESTE DE FERMAT É UM TESTE DA PRIMALIDADE DE UM NÚMERO INTEIRO.
-ELE É FUNDAMENTADO NO CHAMADO PEQUENO TEOREMA DE FERMAT QUE AFIRMA QUE PARA UM DADO NÚMERO PRIMO p E UM INTEIRO a COPRIMO COM ELE a^(p-1) = 1(mod p).
-
-O ALGORITMO FUNCIONA DA SEGUINTE FORMA:
-1. PARA UM DADO NÚMERO n A SER TESTADO, ESCOLHE-SE UM NÚMERO a COPRIMO COM n.
-2. O TEOREMA DE FERMAT É APLICADO AO PAR (a, n) E EM CASO AFIRMATIVO O NÚMERO EM QUESTÃO É POTENCIALMENTE UM PRIMO.
-3. REPETINDO-SE O TESTE UM NÚMERO SUFICIENTEMENTE GRANDE DE VEZES, UMA PRECISÃO MAIOR É GARANTIDA NO TESTE.
-
-*/
+//VAMOS CRIAR UM PROGRAMA QUE TESTA AS ROTINAS CRIADAS NO ARQUIVO fermat_prime_test.h
+//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o fermat_prime_test fermat_prime_test.c -lm -O2
 
 
-//********************************************************************************************************************
-//CABEÇALHO
+
+//Cabeçalho
+#include"fermat_prime_test.h"
 #include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<stdint.h>
-#include<time.h>
-#include<math.h>
-#include"mod_bin_exponentiation.h"
 
-
-//********************************************************************************************************************
-//FUNÇÕES
-
-//Função que implementa o algoritmo de Euclides
-uint64_t gcd_euclides(uint64_t a, uint64_t b){
-if(b==0)
-return a;
-else
-return gcd_euclides(b, a%b);
-
-                                             };
-
-//Teste unitário
-bool test(uint64_t n){
-//Descartando os casos trivais
-if(n%2==0 || n%3==0 || n%5==0)
-return false;
-
-//Semente para geração de números aleatórios,
-srand(time(NULL));
-
-//Variáveis locais
-uint64_t a, result;
-//Procedimentos 
-//Gerando um inteiro aleatória coprimo com o número a ser testado
-pick:
-a=2+rand()%(n-3);
-
-if(gcd_euclides(n, a)!=1)
-goto pick;
-
-//Executando o teste
-result=mod_bin_pow(a, n-1, n);
-
-if(result==1)
-return true;
-if(result!=1)
-return false;
-                     };
-
-//O Teste de Fermat
-bool fermat_test(uint64_t n, int k){
-//Variáveis locais
-int i;
-int p_results=0;
-int f_results=0;//Variável de iteração e contagem de acertos e erros
-
-//Teste
-for(i=0; i<=k;  ++i){
-if(test(n)==false)
-++f_results;
-else if(test(n)==true)
-p_results++;
-                    };
-
-if(f_results>=p_results)
-return false;
-if(f_results<p_results)
-return true;
-                                   };
-
-//********************************************************************************************************************
-//FUNÇÃO PRINCIPAL
+//Função principal
 int main(){
-//Váriaveis
-uint64_t number;
-int k;
-//Procedimentos
-//Recebendo input do usuário
-printf("Digite um número que será testado:\n");
-scanf("%lu", &number);
-printf("Digite um número que define a precisão do teste a ser realizado:\n");
-scanf("%d", &k);
 
-//Executando o teste
-if(fermat_test(number, k)==true)
-printf("Provável primo encontrado!\n");
-else
-printf("Número composto.\n");
+  //Váriaveis
+  uint64_t number, counter=0;
 
-//Finalizando a aplicação
-return 0;
-          }
+  //Procedimentos
+    //Recebendo input do usuário
+    printf("Digite um limitante a sua sequência de prováveis primos:\n");
+    if(scanf("%lu", &number)!=1) return 0;
+
+    //Executando o teste
+    printf("Eis a sequência de (prováveis) primos até %lu:\n", number);
+    for(uint64_t i=2; i<=number; ++i){
+      if(fermat_primality_test(i)==true){
+        printf("%lu, ", i);
+        counter++;
+                                        }
+
+    }
+
+    printf("...\n");
+    printf("Números de primos encontrados: %lu\n", counter);
+
+  //Finalizando a aplicação
+  return 0;
+
+}
