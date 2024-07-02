655ea8508b75cf3d1b227b96c6607f0ccb35778e
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-07-02 00:39:20 -0300

    Update modular_square_root.c

--- modular_square_root.c
+++ modular_square_root.c
@@ -1,38 +1,39 @@
 //VAMOS CRIAR UM PROGRAMA QUE CALCULA RAÍZES QUADRADAS AMENOS DE UMA RELAÇÃO DE CONGRUÊNCIA USANDO TENTATIVA E ERRO
-//COMPILAR ESTE PROGRAMA COM: gcc -o modular_square_root  modular_square_root.c
+//COMPILAR ESTE PROGRAMA COM: gcc -o modular_square_root  modular_square_root.c -lm
 
 
 /*
 UM NÚMERO r É DEFINIDO COMO A RAÍZ QUADRADA DE UM NÚMERO n A MENOS DE UMA CONGRUÊNCIA ENVOLVENDO UM PRIMO p SE r²= n (mod p).
+CLASSES RESIDUAIS QUADRÁTICAS SÃO IMPORTANTES NA MAIORIA DOS ALGORITMOS EM TEORIA DOS NÚMEROS.
 */
 
 //***************************************************************************************************************************
 //CABEÇALHO
 #include<stdio.h>
+#include<math.h>
 #include"mod_bin_exponentiation128.h"
-//VARIÁVEIS GLOBAIS
-#define MAX_SEARCH 10000000000
+
 
 //***************************************************************************************************************************
 //FUNÇÕES
 uint64_t modular_sqrt(uint64_t n, uint64_t p){
 
- //Variáveis locais
- uint64_t i;
- uint64_t tester = mod_bin_pow(n,((p-1)/2), p);
+  //Variáveis locais
+  uint64_t i;
+  uint64_t root=sqrt(n);
+  uint64_t tester = mod_bin_pow(n,((p-1)/2), p);
 
- //Procedimentos
-  //Descartando quadrados não residuais
-  if(tester!=1)
-   return 0;
+  //Procedimentos
+    //Descartando quadrados não residuais
+    if(tester!=1)
+     return 0;
 
-  for(i=n; ; ++i){
-   if(((i*i)%p)==n)
-    return i;
-   if(i>MAX_SEARCH)
-    return 0;
-               };
-                                             };
+    for(i=root; i<n; ++i){
+      if(((i*i)%p)==n) return i;
+    };
+
+  return 0;
+};
 
 
 
@@ -41,29 +42,29 @@ uint64_t modular_sqrt(uint64_t n, uint64_t p){
 
 int main(){
 
- //Variáveis locais
- uint64_t n, p, root;
+  //Variáveis locais
+  uint64_t n, p, root;
 
- //Procedimentos
-  //Recebendo input do usuário
-  printf("Digite um número inteiro: ");
-  scanf("%lu", &p);
+  //Procedimentos
+    //Recebendo input do usuário
+    printf("Digite um número inteiro: ");
+    scanf("%lu", &p);
 
-  printf("Digite um número inteiro: ");
-  scanf("%lu", &n);
-
-
- //Calculando raíz quadrada a menos de uma relação de congruência
-  root = modular_sqrt(n, p);
+    printf("Digite um número inteiro: ");
+    scanf("%lu", &n);
 
 
-  if(root==0)
-   printf("Não foram em contradas raízes quadradas de %lu (mod %lu)\n", n, p);
-  else
-   printf("Raíz quadrada de %lu (mod %lu)=%lu.\n", n, p, root);
+    //Calculando raíz quadrada a menos de uma relação de congruência
+    root = modular_sqrt(n, p);
 
 
- //Finalizando a aplicação
- return 0;
+    if(root==0)
+      printf("Não foram em contradas raízes quadradas de %lu (mod %lu)\n", n, p);
+    else
+      printf("Raíz quadrada de %lu (mod %lu)=%lu.\n", n, p, root);
 
-          }
+
+  //Finalizando a aplicação
+  return 0;
+
+}
