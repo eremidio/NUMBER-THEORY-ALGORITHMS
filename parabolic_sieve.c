a791ed56c7eb6f3ecad707b76c65e3d4c617a66c
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-07-12 17:59:24 -0300

    Update parabolic_sieve.c
    USING BETTER SQUARE DETECTION ALGORITHMS

--- parabolic_sieve.c
+++ parabolic_sieve.c
@@ -1,12 +1,12 @@
-//VAMOS CRIAR UM PROGRAMA QUE IMPLEMENTA O ALGORITMO DA PENEIRA QUADRÁTICA (PARABÓLICIA) PARA COMPUTAR NÚMEROS PRIMOS
-//COMPILAR ESTE PROGRAMA COM O COMANDO: clang -o parabolic_sieve parabolic_sieve.c
+//VAMOS CRIAR UM PROGRAMA QUE IMPLEMENTA O ALGORITMO DA PENEIRA QUADRÁTICA (PARABÓLICA) PARA COMPUTAR NÚMEROS PRIMOS
+//COMPILAR ESTE PROGRAMA COM O COMANDO: clang -o parabolic_sieve parabolic_sieve.c -lm
 
 
 /*
-O ALGORITMO DA PENEIRA QUADRÁTICA (PARABÓLICA) USA A CURVA DE EQUAÇÃO x=y², PARA GERA NÚMEROS PRIMOS. PONTOS NESTA CURVA TEM AS
+O ALGORITMO DA PENEIRA QUADRÁTICA (PARABÓLICA) USA A CURVA DE EQUAÇÃO x=y², PARA GERAR NÚMEROS PRIMOS. PONTOS NESTA CURVA TEM AS
 COORDENADAS (y², y) OU (y², -y). SELECIONANDO OS PONTOS DA CURVA COM COORDENADAS INTEIRAS É POSSÍVEL MOSTRAR QUE OS PONTOS DE 
-INTERSECÇÃO DAS RETAS CONECTANDOS DOIS PONTOS UM ACIMA DO EIXO x (y POSITIVO) E OUTRO ABAIXO DO EIXO x (y NEGATIVO) COM O EIXO x
-TEM COORDENADAS (n, 0) COM n INTEIRO.
+INTERSEÇÃO DAS RETAS CONECTANDOS DOIS PONTOS UM ACIMA DO EIXO x (y POSITIVO) E OUTRO ABAIXO DO EIXO x (y NEGATIVO) COM O EIXO x
+TEM COORDENADA (n, 0) COM n INTEIRO, SENDO n UM QUADRADO PERFEITO OU PRIMO.
 
 PARA MAIORES INFORMAÇÕES: https://mathworld.wolfram.com/QuadraticSieve.html
 */
@@ -14,109 +14,88 @@ PARA MAIORES INFORMAÇÕES: https://mathworld.wolfram.com/QuadraticSieve.html
 
 //**************************************************************************************************************************
 //CABEÇALHO
+#include"prime_power_detection.h"
 #include<stdio.h>
-#include<stdlib.h>
-#include<stdint.h>
-#include<stdbool.h>
+
 
 
 //**************************************************************************************************************************
 //FUNÇÕES
-
-//Função que determina se um  número é quadrado perfeito
-bool perfect_square_check(int64_t n){
-//Variáveis locais
-int64_t odd=1;
-//Procedimentos
-while(n>0){
-n-=odd;
-odd+=2;
-if(n<0)
-return false;
-          };
-
-return true;
-                                    };
-
 //Função que implementa o algoritmo da peneira parábolica
-void parabolic_sieve(int n){
-int x1, x2, y1, y2, intersection;
-double slope;
-int boolean[n];
-int i, j, k;//Variáveis de iteração
 
-//Procedimento
+  //Variáveis locais
+  void parabolic_sieve(int64_t n){
+  int64_t x1, x2, y1, y2, intersection, temp;
+  double slope;
+  bool boolean[n];
+  int64_t i, j, k;//Variáveis de iteração
 
-//TESTE, USE UM // APÓS O FIM DO MESMO
-//printf("Teste 1.\n");
+  //Procedimento
+    //Ajuste do array de booleanos
+    for(k=0; k<n; ++k){
+      if(k==0 || k==1){
+        boolean[k]=false;
+        continue;
+      };
 
-//Ajuste do array de booleanos
-for(k=0; k<n; ++k){
-if(k==0 || k==1){
-boolean[k]=0;
-continue;
-                };
-boolean[k]=1;
-                  };
+      boolean[k]=true;
+    };
 
-//TESTE, USE UM // APÓS O FIM DO MESMO
-//printf("Teste 2.\n");
 
-//Loop principal
-for(i=2; i<n; ++i){
-//Definindo as coordenadas dos pontos acima do eixo x
-y1=i;
-x1=y1*y1;
 
-for(j=2; j<n; ++j){
-//TESTE, USE UM // APÓS O FIM DO MESMO
-//printf("Teste 4.\n");
+    //Loop principal
+    for(i=2; i<n; ++i){
+    
+      //Definindo as coordenadas dos pontos acima do eixo x
+      y1=i; x1=y1*y1;
 
-//Definindo as coordenadas dos pontos abaixo do eixo x
-y2=(-1)*j;
-x2=y2*y2;
+      for(j=2; j<n; ++j){
+        //Definindo as coordenadas dos pontos abaixo do eixo x
+        y2=(-1)*j; x2=y2*y2;
+        if(x1==x2) continue;
 
-if(x1==x2)
-continue;
+        //Calculando a inclinação das retas
+        slope=(x2-x1)/(y2-y1);
 
-//Calculando a inclinação das retas
-slope=(x2-x1)/(y2-y1);
+        //Calculando a intesercção da reta com o ponto
+        intersection = x1-(slope*y1);
+        if(intersection<n) boolean[intersection]=false;
 
-//Calculando a intesercção da reta com o ponto
-intersection = x1-(slope*y1);
-if(intersection<n)
-boolean[intersection]=0;
+      };
 
-                   };
+    };
 
-                   };
 
-//TESTE, USE UM // APÓS O FIM DO MESMO
-//printf("Teste 3.\n");
 
-//Printando os números primos na tela
-printf("Eis a sua lista de primos até %d: ", n);
-for(i=0; i<n; i++){
-if(boolean[i]==1 && perfect_square_check(i)==false)
-printf("%d, ", i);
-                  };
-printf("...\n");
+    //Printando os números primos na tela
+    printf("Eis a sua lista de primos até %li: ", n);
+    for(i=0; i<n; i++){
+      if(boolean[i]==true && fast_square_detection(i, &temp)==false)
+        printf("%li, ", i);
+    };
+
+    printf("...\n");
  
-                            };
+};
 
 //**************************************************************************************************************************
 //FUNÇÃO PRINCIPAL
-
 int main(){
-//DEFINIÇÃO DE VARIÁVEIS
-int number;
-//RECEBENDO INPUT DO USUÁRIO E EXIBINDO OS NÚMEROS PRIMOS
-printf("Vamos calcular os números primos até um certo valor usando a peneira quadrática (parabólica).\n");
-printf("Usuário digite um número inteiro: ");
-scanf("%d", &number);
-parabolic_sieve(number);
+
+  //Variáveis locais
+  int64_t number;
+
+  //Procedimentos  
+    //Recebendo input do usuário
+    printf("Vamos calcular os números primos até um certo valor usando a peneira quadrática (parabólica).\n");
+    printf("Usuário digite um número inteiro: ");
+    scanf("%li", &number);
+  
+    //Calculando primos usando a peneira parabólica
+    parabolic_sieve(number);
 
 
-//FINALIZANDO A APLICAÇÃO
-return 0;
-          }
+  //Finalizando a aplicação
+  return 0;
+
+}
