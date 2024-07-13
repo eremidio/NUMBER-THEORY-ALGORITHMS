f82846af809f8f85b83395466cd5b25b2b2d21b8
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-07-13 00:52:18 -0300

    Update sundaram_sieve.c
    better square detection routines

--- sundaram_sieve.c
+++ sundaram_sieve.c
@@ -1,67 +1,73 @@
-//VAMOS IMPLEMENTAR O ALGORITMO DA PENEIRA DE SUNDARAM  PARA CALCULAR NÚMEROS PRIMOS ARTÉ UM CERTO LIMITE
-//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o sundaram_sieve sundaram_sieve.c 
+//VAMOS IMPLEMENTAR O ALGORITMO DA PENEIRA DE SUNDARAM PARA CALCULAR NÚMEROS PRIMOS ATÉ UM CERTO LIMITE
+//COMPILAR ESTE PROGRAMA COM O COMANDO: clang -o sundaram_sieve sundaram_sieve.c -lm -O2
 
-/*CABEÇALHO*/
-#include<stdbool.h>
-#include<stdint.h>
-#include<stdio.h>
+/*
+PARA MAIORES INFORMAÇÕES: https://canonica.ai/page/Sieve_of_Sundaram
+
+*/
+
+//***********************************************************************************************************************************
+//CABEÇALHO 
+#include"prime_power_detection.h"
 #include<stdlib.h>
-
-//Função que determina se um  número é quadrado perfeito
-bool perfect_square_check(int64_t n){
-//Variáveis locais
-int64_t odd=1;
-//Procedimentos
-while(n>0){
-n-=odd;
-odd+=2;
-if(n<0)
-return false;
-          };
-
-return true;
-                                    };
+#include<stdio.h>
 
 
-//FUNÇÃO QUE APLICA A PENEIRA DE SUNDARAM PARA UMA SEQUÊNCIA DE INTEIROS ATÉ UM DADO VALOR
+//***********************************************************************************************************************************
+//FUNÇÕES
+//Função que implementa a peneira de Sundaram
 void sundaram_sieve(int64_t n){
-//VARIÁVEIS LOCAIS
-bool number_list[n+1];
-int64_t i, j, k;
 
-//PROCEDIMENTOS
-//INICIALIZANDO A LISTA DE BOOLEANOS
-for(k=0; k<n+2; ++k)
-number_list[k]=true;
+  //Vatriáveis locais
+  bool number_list[n+1];
+  int64_t i, j, k;
+  int64_t temp, number;
 
-//APLICANDO A PENEIRA DE SUNDARAM A LISTA DE BOOLEANOS
-for(j=1; j<n; ++j){
-for(i=1; i<j; ++i){
-if((i+j+2*i*j)<n)
-number_list[i+j+2*i*j]=false;
-                  };
-                  };
+  //Procedimentos
+    //Inicializando a a lista de booleanos
+    for(k=0; k<n+2; ++k) number_list[k]=true;
 
-//PRINTANDO A LISTA DE NÚMEROS PRIMOS
-printf("2, 3, ");
-for(k=2; (2*k+1)<n; ++k){
-if(number_list[k]==true && perfect_square_check((2*k+1))==false)
-printf("%li, ", (2*k+1));
-                  };
-                          };
+    //Descartando números compostos
+   for(j=1; j<n; ++j){
+     for(i=1; i<j; ++i){
+       if((i+j+2*i*j)<n) number_list[i+j+2*i*j]=false;
+     };
+   };
+
+   //Exibindo a lista de primos computados
+   printf("2, 3, ");
+   for(k=2; (2*k+1)<n; ++k){
+     number=(2*k+1);
+     if(number_list[k]==true && fast_square_detection(number, &temp)==false)
+       printf("%li, ", number);
+   };
+
+   printf("...\n");
 
 
+};
+
+
+//***********************************************************************************************************************************
 //FUNÇÃO PRINCIPAL
 int main(){
-//DEFINIÇÃO DE VARIÁVEIS
-int64_t n;
-//RECEBENDO INPUT DO USUÁRIO E EXIBINDO OS NÚMEROS PRIMOS
-printf("Vamos calcular os números primos até um certo valor usando a peneira de Sundaram.\n");
-printf("Usuário digite um número inteiro:\n");
-scanf("%li", &n);
-sundaram_sieve(n);
-printf("\n");
 
-//FINALIZANDO A APLICAÇÃO
-return 0;
-          }
+  //Variáveis  locais
+  int64_t n;
+
+
+  //Procedimentos
+    //Recebendo input do9 usuário
+    printf("Vamos calcular os números primos até um certo valor usando a peneira de Sundaram.\n");
+    printf("Usuário digite um número inteiro:\n");
+    if(scanf("%li", &n)!=1) return 0;
+
+    //Calculando números primos usando a peneira de Sundaram    
+    printf("Eis a lista de números primos até %li: \n", n);
+    sundaram_sieve(n);
+
+
+  //Finalizando a aplicação
+  return 0;
+
+}
