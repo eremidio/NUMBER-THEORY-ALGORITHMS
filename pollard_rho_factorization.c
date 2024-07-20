558fc38ece23ccc99f1df542ea7ff1ce5cb227bb
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-07-20 15:26:08 -0300

    Update pollard_rho_factorization.c

--- pollard_rho_factorization.c
+++ pollard_rho_factorization.c
@@ -3,8 +3,8 @@
 
 /*
 O ALGORITMO DE POLLARD É UMA ALGORITMO BEM EFICIENTE PARA DECOMPOR UM NÚMERO INTEIRO EM DOIS FATORES PRIMOS.
-O ALGORITMO BASEIA-SE NA GERAÇÃO GERAÇÃO DE UMA SEQUÊNCIA PSEUDO-ALEATÓRIA USANDO UM POLONÔMIO COMO p(x)=x²+1 OU 
-p(x)=x²+1 mod(n), ONDE n É UM NÚMERO A SER FATORADO.
+O ALGORITMO BASEIA-SE NA GERAÇÃO GERAÇÃO DE UMA SEQUÊNCIA PSEUDO-ALEATÓRIA USANDO UM POLONÔMIO COMO p(x)=x²+1
+OU p(x)=x²+1 mod(n), ONDE n É UM NÚMERO A SER FATORADO.
 
 O ALGORITMO DE POLLARD É EXECUTADO NAS SEGUINTES ETAPAS:
 1. PROVIDO UMA SEMENTE INICIAL x E y O VALOR DE p(x) E p(p(y)) É COMPUTADO.
@@ -27,68 +27,68 @@ PARA MAIORES INFORMAÇÕES: https://en.wikipedia.org/wiki/Pollard%27s_rho_algori
 //FUNÇÕES
 //Algoritmo de Euclides para calcular o mdc de dois inteiros
 uint64_t euclides_check(uint64_t a, uint64_t b){
-//Procedimentos
-if(b==0)
-return a;
-else
-return euclides_check(b, a%b);
+
+  if(b==0) return a;
+  else return euclides_check(b, a%b);
                                                };
 
 //Função que implementa o algoritmo de Pollard
 void pollard_rho(uint64_t x, uint64_t y, uint64_t n){
-//Variáveis locais
-uint64_t factor1, factor2;
-uint64_t remainder=1;
-uint64_t a=1, counter=0;
 
-//Procedimentos
-//Testando inicialmente com x=2, y=2
-restart:
-while(remainder==1){
-x=((x*x+1)%n);
-y=((y*y+1)%n);
-y=((y*y+1)%n);
+  //Variáveis locais
+  uint64_t factor1, factor2;
+  uint64_t remainder=1;
+  uint64_t a=1, counter=0;
 
-if(x>y)
-remainder=euclides_check((x-y), n);
-if(x<y)
-remainder=euclides_check((y-x), n);
+  //Procedimentos
+    //Testando inicialmente com x=2, y=2
+    restart:
+    while(remainder==1){
+      x=((x*x+1)%n);
+      y=((y*y+1)%n);
+      y=((y*y+1)%n);
 
-if(remainder>1 && remainder<n){
-factor1=remainder;
-factor2=n/factor1;
-printf("Fatores não triviais encontrados: %lu e %lu\n", factor1, factor2);
-return;
-                              };
+      if(x>y) remainder=euclides_check((x-y), n);
+      if(x<y) remainder=euclides_check((y-x), n);
 
-//Atualizando variáveis
-counter++;
-if(counter>1e10){
-counter=0;
-a++;
-                };
+      if(remainder>1 && remainder<n){
+        factor1=remainder;
+        factor2=n/factor1;
+        printf("Fatores não triviais encontrados: %lu e %lu\n", factor1, factor2);
+        return;
+      };
 
-if(remainder==n){
-printf("Teste falhou!");
-return;
-                };
-                  };
+      //Atualizando variáveis
+      counter++;
+      if(counter>1e10){
+        counter=0;
+        a++;
+      };
 
-                                                  };
+      if(remainder==n){
+        printf("Teste falhou!");
+        return;
+      };
+    };
+
+};
 
 //*********************************************************************************************************************
 //FUNÇÃO PRINCIPAL
 int main(){
-//Variáveis locais
-uint64_t number;
-//Recebendo o input
-printf("Digite o número a ser fatorado.\n->");
-if(scanf("%li", &number)!=1)
-return 0;
 
-//Aplicando o algoritmo de Pollard rho (p-1)
-pollard_rho(2, 2, number);
+  //Variáveis locais
+  uint64_t number;
 
-//Finalizando a aplicação
-return 0;
-          }
+  //Procedimentos
+    //Recebendo o input
+    printf("Digite o número a ser fatorado: ");
+    if(scanf("%li", &number)!=1) return 0;
+
+    //Aplicando o algoritmo de Pollard rho
+    pollard_rho(2, 2, number);
+
+  //Finalizando a aplicação
+  return 0;
+
+}
