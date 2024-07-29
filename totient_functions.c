b884d47f8d588fb32748910daa44e3e7d1fcaa41
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-07-29 13:40:36 -0300

    Update totient_functions.c

--- totient_functions.c
+++ totient_functions.c
@@ -10,20 +10,25 @@
 
 //Função principal
 int main(){
-//Variáveis locais
-uint64_t number1, number2;
-//Procedimentos
-//Recebendo input do usuário
-printf("Digite um número inteiro: ");
-scanf("%lu", &number1);
-printf("Digite um número inteiro: ");
-scanf("%lu", &number2);
 
-//Testando funções totientes
-printf("λ(%lu)=%lu\n", number1, carmichael_function(number1));
-printf("J(%lu, %lu)=%lu\n", number1, number2, jordan_totient_function(number2, number1));
-printf("Ψ(%lu, %lu)=%Lf\n", number2, number1, dedekind_psi_function(number2, number1));
+  //Variáveis locais
+  uint64_t number1, number2;
 
-//Finalizando a aplicação
-return 0;
-          } 
+
+  //Procedimentos
+    //Recebendo input do usuário
+    printf("Digite um número inteiro: ");
+    scanf("%lu", &number1);
+    printf("Digite um número inteiro: ");
+    scanf("%lu", &number2);
+
+
+    //Testando funções totientes
+    printf("λ(%lu)=%lu\n", number1, carmichael_function(number1));
+    printf("J(%lu, %lu)=%lu\n", number1, number2, jordan_totient_function(number2, number1));
+    printf("Ψ(%lu, %lu)=%Lf\n", number2, number1, dedekind_psi_function(number2, number1));
+
+  //Finalizando a aplicação
+  return 0;
+
+} 
