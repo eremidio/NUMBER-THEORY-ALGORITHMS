4bed57748165eacbf3533174073809ef95df8333
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2025-01-06 00:57:17 -0300

    Update williams_factorization_algebraic.c

--- williams_factorization_algebraic.c
+++ williams_factorization_algebraic.c
@@ -8,24 +8,27 @@
 
 //Função principal
 int main(){
-//Variáveis locais
-char number_string1[40];
-char number_string2[40];
-__int128_t n, E;
-//Procedimento
-//REcebendo input do usuário
-printf("Digite um número que será fatorado: ");
-if(scanf("%s", number_string1)!=1)
-return 0;
-n=scanf128(number_string1);
-printf("Digite um número inteiro auxiliar(expoente): ");
-if(scanf("%s", number_string2)!=1)
-return 0;
-E=scanf128(number_string2);
 
-//Executando o algoritmo
-williams_algebraic_factorization(n, E);
+  //Variáveis locais
+  char number_string1[40];
+  char number_string2[40];
+  __int128_t n, E;
 
-//Finalizando a aplicação
-return 0;
-          }
+
+  //Procedimento
+    //Recebendo input do usuário
+    printf("Digite um número que será fatorado: ");
+    if(scanf("%s", number_string1)!=1) return 0;
+    n=scanf128(number_string1);
+
+    printf("Digite um número inteiro auxiliar(expoente): ");
+    if(scanf("%s", number_string2)!=1) return 0;
+    E=scanf128(number_string2);
+
+    //Executando o algoritmo
+    williams_algebraic_factorization(n, E);
+
+  //Finalizando a aplicação
+  return 0;
+
+}
