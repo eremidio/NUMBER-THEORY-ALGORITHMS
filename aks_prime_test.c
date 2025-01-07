e057fed503005f48ddd142149acec9d100950a19
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2025-01-06 22:17:29 -0300

    Update aks_prime_test.c

--- aks_prime_test.c
+++ aks_prime_test.c
@@ -1,11 +1,9 @@
 //VAMOS CRIAR UM PARA TESTAR AS ROTINAS CRIADAS NO ARQUIVO aks_prime_test.h
-//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o aks_prime_test aks_prime_test.c -lm -lgmp
+//COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o aks_prime_test aks_prime_test.c -lm -O3
 
 
 //Cabeçalho
 #include"aks_prime_test.h"
-#include<stdint.h>
-#include<stdbool.h>
 #include<stdio.h>
 
 
@@ -17,12 +15,13 @@ int main(){
 
   //Procedimentos
     //Testando a primalidade de um número
-    printf("Listas de primos até 3000: ");
-    for(i= 1; i<3001; i++){
-      if(aks_primality_test(i)==true)
+    printf("Listas de primos de até 25000: ");
+    for(i= 2; i<25000; i++){
+      if(aks_primality_test(i, No)==true)
         printf("%lu, ", i);
       };
     printf("...\n");
+
 
   //Finalizando a aplicação
   return 0;
