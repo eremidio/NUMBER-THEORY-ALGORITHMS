0b383c98e0878246a0ba555a482c010bc0855579
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2025-01-17 00:34:48 -0300

    Update mapes_algorithm.c
    new testsit seems to work fine and reproduces corrected computed values

--- mapes_algorithm.c
+++ mapes_algorithm.c
@@ -1,4 +1,4 @@
-//VAMOS CRIAR UM PROGRAMA PARA TESTAR AS  ROTINAS CRIADA NO ARQUIVO mapes_algorithm.h
+//VAMOS CRIAR UM PROGRAMA PARA TESTAR AS ROTINAS CRIADA NO ARQUIVO mapes_algorithm.h
 //COMPILAR ESTE PROGRAMA COM O COMANDO: gcc -o mapes_algorithm mapes_algorithm.c -lm
 
 
@@ -16,6 +16,7 @@ int main(){
 
   //Variáveis locais
   int64_t primes_10[4]={2,3,5,7};
+  int64_t primes_20[8]={2,3,5,7,11,13,17,19};
   int64_t current=1, mapes_value, x;
 
   //Procedimentos
@@ -24,7 +25,7 @@ int main(){
 
 
     //Loop principal
-    for(x=2; x<10000; ++x){
+    for(x=2; x<120; ++x){
       mapes_value=mapes_algorithm(x, 4, primes_10);
       if(mapes_value>current){
         printf("φ(%li, 4)=%li\n", x, mapes_value);
@@ -33,10 +34,13 @@ int main(){
 
     }
 
+    //-------
+    //Teste 2 (página 28)
+    printf("φ(10000, 8)=%li\n", mapes_algorithm(10000, 8, primes_20));
+
 
   //Finalizando a aplicação
   return 0;
 
 }
-
 
